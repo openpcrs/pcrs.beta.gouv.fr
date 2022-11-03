5133d1840e2164de927bc074111fb3b0782b9661
Marine Le Pennec <lepennec.marine@gmail.com>
2022-11-03 16:59:58 +0100

    Add documentation section

---
+++ components/button.js
@@ -0,0 +1,47 @@
+import PropTypes from 'prop-types'
+import Link from 'next/link'
+
+const Button = ({label, size, buttonStyle, href, isExternal, children}) => {
+  if (href) {
+    return (
+      <Link href={href} passHref={isExternal}>
+        <button type='button' aria-label={label} className={`fr-btn fr-btn--${buttonStyle} fr-btn--${size}`}>
+          {children}
+        </button>
+      </Link>
+    )
+  }
+
+  return (
+    <button type='submit' aria-label={label} className={`fr-btn fr-btn--${buttonStyle} fr-btn--${size}`}>
+      {children}
+    </button>
+  )
+}
+
+Button.propTypes = {
+  label: PropTypes.string.isRequired,
+  buttonStyle: PropTypes.oneOf([
+    null,
+    'secondary',
+    'tertiary',
+    'tertiary-no-outline'
+  ]),
+  size: PropTypes.oneOf([
+    'sm',
+    'md',
+    'lg'
+  ]),
+  href: PropTypes.string,
+  isExternal: PropTypes.bool,
+  children: PropTypes.node
+}
+
+Button.defaultProps = {
+  buttonStyle: null,
+  size: 'md',
+  href: null,
+  isExternal: false,
+  children: null
+}
+export default Button
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,52 @@ import Page from '@/layouts/main'
 
 import Hero from '@/components/hero'
 import Section from '@/components/section'
+import Button from '@/components/button'
 
 const Home = () => (
   <Page>
     <Hero />
     <Section background='dark'>
-      <p className='fr-text--sm'>Vous êtes sur le site construit par et pour les territoires. Il expose les différents projets PCRS en cours, leurs acteurs et résultats produits. C’est le support de l’animation nationale associé à la documentation à produire.</p>
+      <p className='fr-text--lg'>Vous êtes sur le site construit par et pour les territoires. Il expose les différents projets PCRS en cours, leurs acteurs et résultats produits. C’est le support de l’animation nationale associé à la documentation à produire.</p>
+    </Section>
+
+    <Section title='Documentation' image='/images/illustrations/doc_illustration.png' id='documentation'>
+      <p>
+        Accompagné par l’ANCT, vous avez tous les éléments en main pour mettre au point le PCRS sur votre territoire. Vous pouvez accéder au contexte réglementaire sur <a className='fr-link fr-icon-external-link-line fr-link--icon-right' href=' https://docs.pcrs.beta.gouv.fr/contexte-reglementaire/reglementation'>cette page</a>.<br />
+        <br />
+        Un ensemble de fiches pratiques est disponible en fonction des problématiques que vous pouvez rencontrer, associées à une foire aux questions pour répondre aux interrogations les plus fréquentes.
+        Si vous ne trouvez pas le contenu correspondant à vos attentes, n’hésitez pas à nous contacter.
+      </p>
+
+      <div className='doc-links'>
+        <ul className='doc-list'>
+          <li><a className='fr-link fr-icon-article-fill fr-link--icon-left' href='https://docs.pcrs.beta.gouv.fr/contexte-reglementaire/geostandards'>Standards</a></li>
+          <li><a className='fr-link fr-icon-article-fill fr-link--icon-left' href='https://docs.pcrs.beta.gouv.fr/contexte-reglementaire/reglementation'>Règlementation</a></li>
+          <li><a className='fr-link fr-icon-article-fill fr-link--icon-left' href='https://docs.pcrs.beta.gouv.fr/organiser-son-projet-pcrs/construction'>Bonnes pratiques</a></li>
+        </ul>
+
+        <Button href='https://docs.pcrs.beta.gouv.fr/' isExternal label='Accéder à la documentation'>Voir la documentation</Button>
+      </div>
+
+      <style jsx>{`
+        p, .doc-links{
+          text-align: left;
+        }
+
+        .doc-links {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          gap: 2em;
+        }
+
+        .doc-list {
+          display: grid;
+          justify-content: center;
+          gap: 10px;
+        }
+      `}</style>
     </Section>
   </Page>
 )
