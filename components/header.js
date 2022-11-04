71b5b934227cbdf6a4a7c52ae61cc7c8fa501b0b
Marine Le Pennec <lepennec.marine@gmail.com>
2022-11-04 17:52:43 +0100

    Fix header responsive

--- components/header.js
+++ components/header.js
@@ -5,26 +5,34 @@ const Header = () => (
     <div className='fr-header__body'>
       <div className='fr-container'>
         <div className='fr-header__body-row'>
-          <Link href='/'>
-            <div className='fr-header__brand fr-enlarge-link'>
-              <div className='fr-header__brand-top'>
-                <div className='fr-header__logo'>
-                  <p className='fr-logo'>
-                    RÉPUBLIQUE
-                    <br />FRANÇAISE
-                  </p>
+          <Link href='/' legacyBehavior>
+            <a>
+              <div className='fr-header__brand fr-enlarge-link'>
+                <div className='fr-header__brand-top'>
+                  <div className='fr-header__logo'>
+                    <p className='fr-logo'>
+                      RÉPUBLIQUE
+                      <br />FRANÇAISE
+                    </p>
+                  </div>
+                </div>
+                <div className='fr-header__service'>
+                  <h1 className='fr-header__service-title'>
+                    Plan Corps de Rue Simplifié
+                  </h1>
                 </div>
               </div>
-              <div className='fr-header__service'>
-                <h1 className='fr-header__service-title'>
-                  Plan Corps de Rue Simplifié
-                </h1>
-              </div>
-            </div>
+            </a>
           </Link>
         </div>
       </div>
     </div>
+
+    <style jsx>{`
+      a {
+        width: 100%;
+      }
+    `}</style>
   </header>
 )
 
