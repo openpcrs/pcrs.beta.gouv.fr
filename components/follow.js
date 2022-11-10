b178388745e76170ca083faac0532fffd1c79655
Guillaume <guillaume@livingdata.co>
2022-11-10 17:20:30 +0100

    Use dsfr, remove input & prepare for sendinblue iframe

--- components/follow.js
+++ components/follow.js
@@ -1,8 +1,7 @@
+import {useState} from 'react'
+
 const Follow = () => {
-  const handleSubmit = e => {
-    e.preventDefault()
-    console.log('Enregistrement à la Newsletter effectué!')
-  }
+  const [isShown, setIsShown] = useState(false)
 
   return (
     <div className='fr-follow'>
@@ -10,80 +9,65 @@ const Follow = () => {
         <div className='fr-grid-row'>
           <div className='fr-col-12 fr-col-md-8'>
             <div className='fr-follow__newsletter'>
-              <h5 className='fr-h5 fr-follow__title'>Abonnez-vous à notre lettre d’information</h5>
               <div>
-                <form onSubmit={handleSubmit}>
-                  <label className='fr-label' htmlFor='newsletter-email'>
-                    Votre adresse électronique (ex. : nom@domaine.fr)
-                  </label>
-                  <div className='fr-input-wrap fr-input-wrap--addon'>
-                    <input
-                      className='fr-input'
-                      aria-describedby='fr-newsletter-hint-text'
-                      title='Votre adresse électronique (ex. : nom@domaine.fr)'
-                      placeholder='Votre adresse électronique (ex. : nom@domaine.fr)'
-                      type='email'
-                      id='newsletter-email'
-                      name='newsletter-email'
-                    />
-                    <button
-                      className='fr-btn'
-                      id='newsletter-button'
-                      title='S’abonner à notre lettre d’information'
-                      value='submit'
-                      type='submit'
-                    >
-                      S’abonner
-                    </button>
-                  </div>
-                  <p id='fr-newsletter-hint-text' className='fr-hint-text'>
-                    En renseignant votre adresse électronique, vous acceptez de recevoir nos actualités par courriel. Vous pouvez vous désinscrire à tout moment à l’aide des liens de désinscription ou en nous contactant.
-                  </p>
-                </form>
+                <h5 className='fr-h5 fr-follow__title'>Abonnez-vous à notre lettre d’information</h5>
+                <p className='fr-text--sm fr-follow__desc'>
+                  En renseignant votre adresse électronique, vous acceptez de recevoir nos actualités par courriel. Vous pouvez vous désinscrire à tout moment à l’aide des liens de désincription ou en nous contactant.
+                </p>
               </div>
+              <div>
+                <button
+                  className='fr-btn'
+                  title='S’abonner à notre lettre d’information'
+                  type='button'
+                  onClick={() => setIsShown(!isShown)}
+                >
+                  S’abonner <span className={isShown ? 'fr-icon-arrow-up-s-line' : 'fr-icon-arrow-down-s-line'} />
+
+                </button>
+              </div>
+              {isShown && (
+                <p>Prochainement...</p>
+              )}
             </div>
           </div>
           <div className='fr-col-12 fr-col-md-4'>
             <div className='fr-follow__social'>
-              <p className='fr-h5 fr-mb-3v fr-mb-3v'>
-                Suivez-nous<br /> sur les réseaux sociaux
+              <p className='fr-h5 fr-mb-3v'>
+                Suivez-nous <br /> sur les réseaux sociaux
               </p>
-              <div className='social-media'>
-                <a
-                  className='fr-link--twitter fr-link'
-                  title='Nous suivre sur Twitter'
-                  href='https://twitter.com/pcrsbeta'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  Twitter
-                </a>
-                <a
-                  className='fr-link--linkedin fr-link'
-                  title='Nous suivre sur Linkedin'
-                  href='https://www.linkedin.com/company/pcrs-beta-gouv-fr/about/'
-                  target='_blank'
-                  rel='noreferrer'
-                >
-                  Linkedin
-                </a>
-              </div>
+              <ul className='fr-links-group fr-links-group--lg'>
+                <li>
+                  <a
+                    className='fr-link--twitter fr-link'
+                    title='Nous suivre sur Twitter'
+                    href='https://twitter.com/pcrsbeta'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    Twitter
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className='fr-link--linkedin fr-link'
+                    title='Nous suivre sur Linkedin'
+                    href='https://www.linkedin.com/company/pcrs-beta-gouv-fr/about/'
+                    target='_blank'
+                    rel='noreferrer'
+                  >
+                    Linkedin
+                  </a>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
       </div>
 
       <style jsx>{`
-        .fr-follow__newsletter {
-          gap: 1em;
-        }
-
-        .fr-follow__social {
-          justify-content: flex-start;
-        }
-
-        .fr-follow__title {
-          width: 100%;
+        .fr-follow {
+          text-align: left;
         }
       `}</style>
     </div>
