ed530433ac526bd3a97cf4a88441f081d78a2af5
Guillaume <guillaume@livingdata.co>
2022-11-10 15:07:30 +0100

    Add legal

---
+++ pages/mentions-legales.js
@@ -0,0 +1,63 @@
+import Page from '@/layouts/main'
+
+const MentionsLegales = () => (
+  <Page>
+    <section className='fr-p-3w'>
+      <h2>Disponibilité du site</h2>
+      <p>
+        L’éditeur s’engage à mettre en œuvre tout ce qui est possible techniquement pour sécuriser l’accès et l’utilisation du site pcrs.beta.gouv.fr. Elle est accessible 24 heures sur 24, 7 jours sur 7, sauf en cas de force majeure ou de survenance d’un événement hors du contrôle de l’éditeur et sous réserve d’éventuelles pannes et interventions de maintenance nécessaires au bon fonctionnement de la plateforme.
+      </p>
+      <p>
+        L’éditeur ne garantit pas que la plateforme fonctionne de manière ininterrompue, sécurisée ou qu’elle soit exempte d’erreurs. Les interventions de maintenance pourront être effectuées sans que les utilisateurs de pcrs.beta.gouv.fr n’aient été préalablement avertis.
+      </p>
+      <p>
+        L’éditeur ne peut être tenu pour responsable d’une éventuelle rupture de ce service ou d’un problème technique empêchant un utilisateur d’accéder au site pcrs.beta.gouv.fr.
+      </p>
+      <p>
+        L’éditeur se réserve la possibilité de refuser l’accès aux services offerts par pcrs.beta.gouv.fr en cas d’usage abusif ou de violation de dispositions législatives ou réglementaires en vigueur.
+      </p>
+      <h2>Données personnelles</h2>
+      <p>
+        Les données recueillies ou traitées sont hébergées en France ou en Union Européenne.
+      </p>
+      <p>
+        En application de la loi n°78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés, les utilisateurs de la plateforme pcrs.beta.gouv.fr disposent notamment d’un droit d’accès et de rectification auprès de l’éditeur de la plateforme.
+      </p>
+      <p>
+        Ce droit s’exerce auprès de l’Agence Nationale de la Cohésion des Territoires, 20 avenue de Ségur, 75007 Paris. <br />dpo [à] anct.gouv.fr
+      </p>
+    </section>
+    <section className='fr-p-3w'>
+      <h2>Nous contacter</h2>
+      <p>
+        contact@pcrs.beta.gouv.fr
+      </p>
+      <h2>Mentions légales</h2>
+      <h3>Éditeur</h3>
+      <p>
+        Agence Nationale de la Cohésion des Territoires 20, avenue de Ségur 75007 Paris
+      </p>
+      <p>
+        info [à] anct.gouv.fr
+      </p>
+      <p>
+        Directeur de publication : Yves Le Breton, ANCT
+      </p>
+      <h3>Hébergeur</h3>
+      <p>
+        Scalingo SAS
+        <br />
+        3, place de Haguenau
+        <br />
+        67000 Strasbourg
+        <br />
+        France
+        <br />
+        SIRET 80866548300018
+      </p>
+    </section>
+  </Page>
+)
+
+export default MentionsLegales
+
