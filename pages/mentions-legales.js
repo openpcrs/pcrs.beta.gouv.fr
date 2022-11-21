b944edc521948dc2b7d43837003d498faaa988a5
Guillaume <guillaume@livingdata.co>
2022-11-21 12:00:46 +0100

    Add Section

--- pages/mentions-legales.js
+++ pages/mentions-legales.js
@@ -1,10 +1,12 @@
 import Page from '@/layouts/main'
+import Section from '@/components/section'
 
 const MentionsLegales = () => (
   <Page title='Mentions légales' description='Mentions légales relatives au site pcrs.beta.gouv.fr'>
-    <section className='fr-p-3w'>
-      <h1>Mentions légales</h1>
-      <h2>Données personnelles</h2>
+    <Section
+      title='Mentions légales'
+    >
+      <h4>Données personnelles</h4>
       <p>
         Les données recueillies ou traitées sont hébergées en France ou en Union Européenne.
       </p>
@@ -14,13 +16,11 @@ const MentionsLegales = () => (
       <p>
         Ce droit s’exerce auprès de l’Agence Nationale de la Cohésion des Territoires, 20 avenue de Ségur, 75007 Paris. <br />dpo [à] anct.gouv.fr
       </p>
-    </section>
-    <section className='fr-p-3w'>
-      <h2>Nous contacter</h2>
+      <h4>Nous contacter</h4>
       <p>
         <a href='mailto:contact&#64;pcrs.beta.gouv.fr'>contact&#64;pcrs.beta.gouv.fr</a>
       </p>
-      <h3>Éditeur</h3>
+      <h5>Éditeur</h5>
       <p>
         Agence Nationale de la Cohésion des Territoires <br /> 20, avenue de Ségur 75007 Paris
       </p>
@@ -30,20 +30,14 @@ const MentionsLegales = () => (
       <p>
         Directeur de publication : Yves Le Breton, ANCT
       </p>
-      <h3>Hébergeur</h3>
+      <h5>Hébergeur</h5>
       <p>
         société Scalingo SAS <br /> Code APE 6311Z
       </p>
       <p>
         Siège social : <br /> 3, place de Haguenau <br /> 67000 Strasbourg - France
       </p>
-    </section>
-    <style jsx>{`
-      section {
-        max-width: 1000px;
-        margin: auto;
-      }
-    `}</style>
+    </Section>
   </Page>
 )
 
