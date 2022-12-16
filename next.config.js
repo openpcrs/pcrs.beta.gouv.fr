466da5f5814943cf0aaaf8c7582d4c158af10c3d
Marine Le Pennec <lepennec.marine@gmail.com>
2022-12-16 18:21:08 +0100

    Prepare Ghost api calls

---
+++ next.config.js
@@ -0,0 +1,17 @@
+import bundleAnalyzer from '@next/bundle-analyzer'
+
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true'
+})
+
+const imagesDomains = ['static.data.gouv.fr']
+
+if (process.env.NEXT_PUBLIC_GHOST_URL_IMAGES_SOURCE) {
+  imagesDomains.push(process.env.NEXT_PUBLIC_GHOST_URL_IMAGES_SOURCE)
+}
+
+withBundleAnalyzer({
+  images: {
+    domains: imagesDomains
+  }
+})
