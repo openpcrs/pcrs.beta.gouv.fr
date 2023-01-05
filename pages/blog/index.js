599c615e67ac6c4eebc8afcfd8d0167264f109ab
Marine Le Pennec <lepennec.marine@gmail.com>
2023-01-05 14:59:21 +0100

    Display error when blog unavailable

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,6 +3,8 @@ import Image from 'next/image'
 
 import {getPosts, getTags} from '@/lib/blog'
 
+import colors from '@/styles/colors'
+
 import Page from '@/layouts/main'
 
 import BlogPagination from '@/components/blog/blog-pagination'
@@ -11,29 +13,55 @@ import BlogTags from '@/components/blog/blog-tags'
 
 const Blog = ({posts, tags, tagsList, pagination}) => (
   <Page title='Blog du PCRS' description='La liste complète des billets du blog autour du PCRS'>
-    <div className='blog-header fr-my-5w'>
-      <Image
-        src='/images/illustrations/blog_illustration.png'
-        height={200}
-        width={200}
-        alt=''
-        aria-hidden='true'
-      />
-      <h2 className='fr-mt-5w fr-mb-0'>Blog du PCRS</h2>
-    </div>
-    <div className='blog-posts-wrapper'>
-      <div className='blog-posts-container'>
-        {tagsList.length > 0 && <BlogTags selectedTags={tags} tagsList={tagsList} />}
-
-        <div className='blog-cards-list'>
-          {posts.length > 0 && posts.map(post => <BlogCard key={post.id} post={post} />)}
-          {(posts.length === 0 && tags.length === 0) && <div className='no-article'>Aucun article de blog n’est disponible</div>}
-          {(posts.length === 0 && tags.length > 0) && <div className='no-article'>Aucun article ne contient ces tags</div>}
+    {posts ? (
+      <>
+        <div className='blog-header fr-my-5w'>
+          <Image
+            src='/images/illustrations/blog_illustration.png'
+            height={200}
+            width={200}
+            alt=''
+            aria-hidden='true'
+          />
+          <h2 className='fr-mt-5w fr-mb-0'>Blog du PCRS</h2>
         </div>
 
-        <BlogPagination {...pagination} />
+        <div className='blog-posts-wrapper'>
+          <div className='blog-posts-container'>
+            {tagsList.length > 0 && <BlogTags selectedTags={tags} tagsList={tagsList} />}
+
+            <div className='blog-cards-list'>
+              {posts.length > 0 && posts.map(post => <BlogCard key={post.id} post={post} />)}
+              {(posts.length === 0 && tags.length === 0) && <div className='no-article'>Aucun article de blog n’est disponible</div>}
+              {(posts.length === 0 && tags.length > 0) && <div className='no-article'>Aucun article ne contient ces tags</div>}
+            </div>
+
+            <BlogPagination {...pagination} />
+          </div>
+        </div>
+      </>
+    ) : (
+      <div className='unavailable fr-p-5w'>
+        <Image
+          src='/images/illustrations/500.png'
+          height={456}
+          width={986}
+          alt=''
+          style={{
+            width: '100%',
+            maxWidth: '500px',
+            height: 'auto'
+          }}
+        />
+
+        <div className='fr-pt-8w'>
+          <p>
+            <b className='fr-h3'>Le blog est actuellement inaccessible</b><br />
+            <i>Merci de réessayer ultérieurement</i>
+          </p>
+        </div>
       </div>
-    </div>
+    )}
 
     <style jsx>{`
       .blog-posts-wrapper {
@@ -63,6 +91,11 @@ const Blog = ({posts, tags, tagsList, pagination}) => (
 
       .no-article {
         font-style: italic;
+      }
+
+      .unavailable {
+        text-align: center;
+        color: ${colors.darkgrey};
       }
 
       @media (max-width: 365px) {
