c4cb4109fa881fd473ac9e2d02fd4129b5f89eaa
Guillaume <guillaume@livingdata.co>
2023-01-17 10:04:54 +0100

    Add fakeproject colors and contours

--- components/map/map.js
+++ components/map/map.js
@@ -1,13 +1,78 @@
-import {useEffect, useRef, useState} from 'react'
+import {useCallback, useEffect, useRef, useState} from 'react'
 import PropTypes from 'prop-types'
 import maplibreGl from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
+import projets from '../../fakeprojets.json'
+import departementData from './sources/departements.json'
+import departementFillLayer from './layers/departement-fill.json'
+import departementLayer from './layers/departement-layer.json'
 
 import vector from './styles/vector.json'
 
-const Map = ({isMobile}) => {
+const getPCRSFeatures = projects => {
+  const featurePcrs = []
+
+  projects.forEach(p => {
+    const territoires = new Set(p.perimetre.territoires.map(t => (
+      t.codeDepartement
+    )))
+
+    const pcrs = {
+      id: p.id,
+      perimetre: territoires,
+      statut: p.statut
+    }
+
+    featurePcrs.push(pcrs)
+  })
+
+  return featurePcrs
+}
+
+function loadLayer({map, data, layer, sourceName}) {
+  if (!map.getSource(sourceName)) {
+    map.addSource(sourceName, {
+      type: 'geojson',
+      data,
+      generateId: true
+    })
+  }
+
+  if (!map.getLayer(layer.id)) {
+    map.addLayer(layer)
+  }
+}
+
+const Map = ({handleClick, isMobile}) => {
   const [map, setMap] = useState()
   const mapNode = useRef(null)
+  const pcrsFeatures = getPCRSFeatures(projets)
+
+  const popupRef = useRef(new maplibreGl.Popup({
+    offset: 50,
+    maxWidth: 'none',
+    closeButton: false,
+    closeOnClick: false
+  }))
+
+  const getFeatures = useCallback(() => {
+    const features = []
+
+    departementData.features.forEach(f => {
+      pcrsFeatures.forEach(t => {
+        if (t.perimetre.has(f.properties.code)) {
+          features.push(f)
+          f.properties.pcrsId = t.id
+          f.properties.pcrsStatus = t.statut
+        }
+      })
+    })
+
+    return {
+      type: 'FeatureCollection',
+      features
+    }
+  }, [pcrsFeatures])
 
   useEffect(() => {
     const node = mapNode.current
@@ -28,6 +93,7 @@ const Map = ({isMobile}) => {
       maplibreMap.addControl(new maplibreGl.AttributionControl({compact: true}), 'bottom-right')
 
       setMap(maplibreMap)
+      console.log('set map')
 
       return () => {
         resizer.disconnect()
@@ -35,6 +101,66 @@ const Map = ({isMobile}) => {
       }
     }
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    let hoveredCode = null
+    let selectedCodes = null
+    const features = getFeatures()
+
+    if (map) {
+      map.on('styledata', () => {
+        loadLayer({
+          map,
+          data: features,
+          layer: departementFillLayer,
+          sourceName: 'departements'
+        })
+      })
+
+      map.on('styledata', () => {
+        loadLayer({
+          map,
+          data: features,
+          layer: departementLayer,
+          sourceName: 'contours-departements'
+        })
+      })
+
+      map.on('click', 'departements-fills', e => {
+        handleClick(e)
+      })
+
+      map.on('click', 'departements-fills', e => {
+        if (e.features.length > 0) {
+          if (selectedCodes !== null) {
+            map.setFeatureState(
+              {source: 'departements', id: selectedCodes},
+              {hover: false}
+            )
+          }
+
+          selectedCodes = e.features[0].id
+
+          map.setFeatureState(
+            {source: 'departements', id: selectedCodes},
+            {hover: true}
+          )
+        }
+      })
+
+      map.on('mouseleave', 'departements-fills', () => {
+        if (hoveredCode !== null) {
+          map.setFeatureState(
+            {source: 'departements', id: hoveredCode}
+          )
+
+          popupRef.current.remove()
+        }
+
+        hoveredCode = null
+      })
+    }
+  }, [map, getFeatures, handleClick, isMobile])
 
   return (
     <div style={{position: 'relative', height: '100%', width: '100%'}}>
@@ -44,6 +170,7 @@ const Map = ({isMobile}) => {
 }
 
 Map.propTypes = {
+  handleClick: PropTypes.func,
   isMobile: PropTypes.bool
 }
 
--- layouts/map.js
+++ layouts/map.js
@@ -1,14 +1,26 @@
 import {useState, useContext} from 'react'
 
+import {getProject} from '@/lib/pcrs.js'
 import DeviceContext from '@/contexts/device.js'
 
 import Map from '@/components/map/map.js'
 
 export const Mobile = () => {
   const {viewHeight} = useContext(DeviceContext)
-  const isOpen = false
+  const [isOpen, setIsOpen] = useState(false)
+  const [project, setProject] = useState()
 
-  console.log(isOpen)
+  const handleClick = e => {
+    const projectId = getProject(e.features[0].properties.pcrsId)
+    setProject(projectId)
+    setIsOpen(true)
+  }
+
+  const handleTitleClick = () => {
+    if (project) {
+      setIsOpen(!isOpen)
+    }
+  }
 
   return (
     <div
@@ -24,7 +36,64 @@ export const Mobile = () => {
           width: '100%'
         }}
       >
-        <Map isMobile />
+        <Map isMobile handleClick={handleClick} />
+      </div>
+      <div
+        style={{
+          height: isOpen ? viewHeight - 147 : '46px',
+          overflowY: isOpen ? 'auto' : 'hidden',
+          overflowX: 'hidden',
+          padding: '2px',
+          border: '1px solid lightgrey',
+          borderBottom: '0'
+        }}
+      >
+        <div
+          className='fr-p-3v'
+          style={{
+            float: isOpen ? 'right' : 'left',
+            width: '100%',
+            display: 'flex',
+            justifyContent: project && isOpen ? 'end' : 'space-between'
+          }}
+        >
+          {isOpen && project ? (
+            <span
+              className='fr-icon--lg fr-icon-close-circle-line'
+              style={{
+                color: 'white',
+                cursor: 'pointer'
+              }}
+              onClick={handleTitleClick}
+            />
+          ) : (
+            (!isOpen && project) ? (
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  width: '100%'
+                }}
+              >
+                <span>{project?.nom}</span>
+                <span
+                  className='fr-icon--lg fr-icon-arrow-up-s-line'
+                  aria-hidden='true'
+                  style={{
+                    cursor: 'pointer'
+                  }}
+                  onClick={handleTitleClick}
+                />
+              </div>
+            ) : (
+              <small>Sélectionnez un PCRS sur la carte</small>
+            )
+          )}
+        </div>
+        {isOpen && (
+          <div>test</div>
+        )}
       </div>
     </div>
   )
@@ -32,6 +101,13 @@ export const Mobile = () => {
 
 export const Desktop = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const [project, setProject] = useState()
+
+  const handleClick = e => {
+    const projectId = getProject(e.features[0].properties.pcrsId)
+    setProject(projectId)
+    setIsOpen(true)
+  }
 
   return (
     <div
@@ -40,41 +116,43 @@ export const Desktop = () => {
         contain: 'content'
       }}
     >
-      <div
-        style={{
-          minWidth: isOpen ? '460px' : '5px',
-          maxWidth: '460px',
-          boxShadow: '0px 0px 5px grey',
-          height: 'calc(100vh - 117px)',
-          zIndex: 1,
-          overflow: 'auto',
-          overflowX: 'hidden'
-        }}
-      >
-        <button
-          type='button'
-          className={`fr-icon-arrow-${isOpen ? 'left' : 'right'}-s-line`}
+      {project && (
+        <div
           style={{
-            position: 'absolute',
-            top: '25px',
-            left: isOpen ? '460px' : '5px',
-            backgroundColor: 'white',
-            height: '50px',
-            width: '30px',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            borderRadius: '0 5px 5px 0',
-            boxShadow: '2px 2px 5px grey'
+            minWidth: isOpen ? '460px' : '5px',
+            maxWidth: '460px',
+            boxShadow: '0px 0px 5px grey',
+            height: 'calc(100vh - 117px)',
+            zIndex: 1,
+            overflow: 'auto',
+            overflowX: 'hidden'
           }}
-          onClick={() => setIsOpen(!isOpen)}
-        />
-        {isOpen && (
-          <div>test</div>
-        )}
-      </div>
+        >
+          <button
+            type='button'
+            className={`fr-icon-arrow-${isOpen ? 'left' : 'right'}-s-line`}
+            style={{
+              position: 'absolute',
+              top: '25px',
+              left: isOpen ? '460px' : '5px',
+              backgroundColor: 'white',
+              height: '50px',
+              width: '30px',
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              borderRadius: '0 5px 5px 0',
+              boxShadow: '2px 2px 5px grey'
+            }}
+            onClick={() => setIsOpen(!isOpen)}
+          />
+          {isOpen && (
+            <div>test</div>
+          )}
+        </div>
+      )}
       <div style={{width: '100%', height: 'calc(100vh - 117px)'}}>
-        <Map />
+        <Map handleClick={handleClick} />
       </div>
     </div>
   )
