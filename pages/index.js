8b77f030c36c6f8e48f0a72a5998c8c1efdb3cbc
Guillaume <guillaume@livingdata.co>
2023-01-30 09:23:06 +0100

    Remove iframe maps

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import {useState} from 'react'
 import Image from 'next/image'
 
 import Page from '@/layouts/main.js'
@@ -10,275 +9,269 @@ import SectionImage from '@/components/section-image.js'
 import Section from '@/components/section.js'
 import Button from '@/components/button.js'
 import Follow from '@/components/follow.js'
-import PcrsIframeMap from '@/components/pcrs-iframe-map.js'
 import EventCarousel from '@/components/event-carousel.js'
 
-const Home = () => {
-  const [isMapShown, setIsMapShown] = useState(false)
+const Home = () => (
+  <Page>
+    <EventCarousel />
+    <Hero />
+    <>
+      <p className='intro fr-p-4w fr-p-md-9w fr-text--xl'>
+        Vous êtes sur le site construit par et pour les territoires. Il expose les différents projets PCRS en cours, leurs acteurs et résultats produits. C’est le support de l’animation nationale associé à la documentation à produire.
+      </p>
 
-  return (
-    <Page>
-      <EventCarousel />
-      <Hero />
-      <>
-        <p className='intro fr-p-4w fr-p-md-9w fr-text--xl'>
-          Vous êtes sur le site construit par et pour les territoires. Il expose les différents projets PCRS en cours, leurs acteurs et résultats produits. C’est le support de l’animation nationale associé à la documentation à produire.
-        </p>
+      <style jsx>{`
+        .intro {
+          background: ${colors.darkgrey};
+          color: white;
+          text-align: center;
+        }
+      `}</style>
+    </>
 
-        <style jsx>{`
-          .intro {
-            background: ${colors.darkgrey};
-            color: white;
-            text-align: center;
-          }
-        `}</style>
-      </>
+    <SectionImage
+      title='Documentation'
+      imageLink='/images/illustrations/doc_illustration.png'
+      id='documentation'
+    >
+      <p>
+        Accompagné par l’ANCT, vous avez tous les éléments en main pour mettre au point le PCRS sur votre territoire. Vous pouvez accéder au contexte réglementaire sur <a className='fr-link fr-icon-external-link-line fr-link--icon-right' href=' https://docs.pcrs.beta.gouv.fr/contexte-reglementaire/reglementation'>cette page</a>.
+      </p>
+      <p>
+        Un ensemble de fiches pratiques est disponible en fonction des problématiques que vous pouvez rencontrer, associées à une foire aux questions pour répondre aux interrogations les plus fréquentes.
+        Si vous ne trouvez pas le contenu correspondant à vos attentes, n’hésitez pas à nous contacter.
+      </p>
 
-      <SectionImage
-        title='Documentation'
-        imageLink='/images/illustrations/doc_illustration.png'
-        id='documentation'
-      >
-        <p>
-          Accompagné par l’ANCT, vous avez tous les éléments en main pour mettre au point le PCRS sur votre territoire. Vous pouvez accéder au contexte réglementaire sur <a className='fr-link fr-icon-external-link-line fr-link--icon-right' href=' https://docs.pcrs.beta.gouv.fr/contexte-reglementaire/reglementation'>cette page</a>.
-        </p>
-        <p>
-          Un ensemble de fiches pratiques est disponible en fonction des problématiques que vous pouvez rencontrer, associées à une foire aux questions pour répondre aux interrogations les plus fréquentes.
-          Si vous ne trouvez pas le contenu correspondant à vos attentes, n’hésitez pas à nous contacter.
-        </p>
-
-        <div className='doc-links'>
-          <ul className='doc-list'>
-            <li>
-              <a className='fr-link fr-icon-article-fill fr-link--icon-left' href='https://docs.pcrs.beta.gouv.fr/contexte-reglementaire/geostandards'>
-                Standards
-              </a>
-            </li>
-            <li>
-              <a className='fr-link fr-icon-article-fill fr-link--icon-left' href='https://docs.pcrs.beta.gouv.fr/contexte-reglementaire/reglementation'>
-                Règlementation
-              </a>
-            </li>
-            <li>
-              <a className='fr-link fr-icon-article-fill fr-link--icon-left' href='https://docs.pcrs.beta.gouv.fr/organiser-son-projet-pcrs/construction'>
-                Bonnes pratiques
-              </a>
-            </li>
-          </ul>
-
-          <Button
-            isExternal
-            href='https://docs.pcrs.beta.gouv.fr/'
-            label='Accéder à la documentation'
-          >
-            Voir la documentation
-          </Button>
-        </div>
-
-        <style jsx>{`
-          p, .doc-links{
-            text-align: left;
-          }
-
-          .doc-links {
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-            gap: 2em;
-          }
-
-          .doc-list {
-            display: grid;
-            justify-content: center;
-            gap: 10px;
-          }
-        `}</style>
-      </SectionImage>
-
-      <SectionImage
-        title='Suivi géographique'
-        subtitle=''
-        background='secondary'
-        imageLink='/images/illustrations/geo_illustration.png'
-        imageSide='right'
-        id='suivi-geo'
-        panelBottom={isMapShown ? <PcrsIframeMap /> : null}
-      >
-        <p>
-          De nombreux territoires ont déjà mené à terme des projets PCRS ou entrepris les démarches pour être prêts pour l’échéance de complétude de 2026.
-        </p>
-        <p>
-          Une cartographie vous montre quelles zones sont déjà pourvues de données raster ou vecteur, programmées ou en cours de réflexion. C’est un outil précieux pour identifier si votre territoire est déjà concerné ou doit faire l’objet d’une réflexion pour construire un PCRS.
-        </p>
-        <p>
-          Ces cartes que nous reprenons ci-dessous sont régulièrement mises à jour par l’IGN ou au gré des déclarations spontanées des autorités publiques auprès de leur service.
-        </p>
-
-        <Button
-          label='Consulter la carte d’état d’avancement du PCRS'
-          onClick={() => setIsMapShown(!isMapShown)}
-        >
-          Consulter la carte d’état d’avancement du PCRS <span className={isMapShown ? 'fr-icon-arrow-up-s-line' : 'fr-icon-arrow-down-s-line'} />
-        </Button>
-
-        <style jsx>{`
-          p {
-            text-align : left
-          }
-
-          .iframes {
-            width: 100%;
-          }
-        `}</style>
-      </SectionImage>
-
-      <SectionImage
-        title='Feuille de route'
-        subtitle='Les étapes suivantes marqueront le développement de ce portail'
-        imageLink='/images/illustrations/progress_illustration.png'
-        id='feuille-de-route'
-      >
-        <ul className='fr-text--sm dev-list'>
+      <div className='doc-links'>
+        <ul className='doc-list'>
           <li>
-            <span className='fr-icon-git-branch-fill' /> Mise en ligne d’une cartographie des projets et d’un annuaire des APLC complétés de manière collaborative.
+            <a className='fr-link fr-icon-article-fill fr-link--icon-left' href='https://docs.pcrs.beta.gouv.fr/contexte-reglementaire/geostandards'>
+              Standards
+            </a>
           </li>
           <li>
-            <span className='fr-icon-git-branch-fill' /> Consolidation de la documentation en continu
+            <a className='fr-link fr-icon-article-fill fr-link--icon-left' href='https://docs.pcrs.beta.gouv.fr/contexte-reglementaire/reglementation'>
+              Règlementation
+            </a>
           </li>
           <li>
-            <span className='fr-icon-git-branch-fill' /> Construction d’un cadre de financement commun
-          </li>
-          <li>
-            <span className='fr-icon-git-branch-fill' /> Mise en valeur des PCRS d’ores et déjà disponibles sous licence ouverte.
+            <a className='fr-link fr-icon-article-fill fr-link--icon-left' href='https://docs.pcrs.beta.gouv.fr/organiser-son-projet-pcrs/construction'>
+              Bonnes pratiques
+            </a>
           </li>
         </ul>
 
-        <style jsx>{`
-          .dev-list {
-            text-align: left;
-            display: grid;
-            justify-content: center;
-            gap: 10px;
-          }
-        `}</style>
-      </SectionImage>
+        <Button
+          isExternal
+          href='https://docs.pcrs.beta.gouv.fr/'
+          label='Accéder à la documentation'
+        >
+          Voir la documentation
+        </Button>
+      </div>
 
-      <Section
-        title='Événements autour du PCRS'
-        background='blue'
-        id='evenements'
-        style={{
-          textAlign: 'center'
-        }}
+      <style jsx>{`
+        p, .doc-links{
+          text-align: left;
+        }
+
+        .doc-links {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          gap: 2em;
+        }
+
+        .doc-list {
+          display: grid;
+          justify-content: center;
+          gap: 10px;
+        }
+      `}</style>
+    </SectionImage>
+
+    <SectionImage
+      title='Suivi géographique'
+      subtitle=''
+      background='secondary'
+      imageLink='/images/illustrations/geo_illustration.png'
+      imageSide='right'
+      id='suivi-geo'
+    >
+      <p>
+        De nombreux territoires ont déjà mené à terme des projets PCRS ou entrepris les démarches pour être prêts pour l’échéance de complétude de 2026.
+      </p>
+      <p>
+        Une cartographie vous montre quelles zones sont déjà pourvues de données raster ou vecteur, programmées ou en cours de réflexion. C’est un outil précieux pour identifier si votre territoire est déjà concerné ou doit faire l’objet d’une réflexion pour construire un PCRS.
+      </p>
+      <p>
+        Ces cartes que nous reprenons ci-dessous sont régulièrement mises à jour par l’IGN ou au gré des déclarations spontanées des autorités publiques auprès de leur service.
+      </p>
+
+      <Button
+        label='Consulter la carte d’état d’avancement du PCRS'
+        href='/suivi-pcrs'
       >
+        Consulter la carte d’état d’avancement du PCRS
+      </Button>
+
+      <style jsx>{`
+        p {
+          text-align : left
+        }
+
+        .iframes {
+          width: 100%;
+        }
+      `}</style>
+    </SectionImage>
+
+    <SectionImage
+      title='Feuille de route'
+      subtitle='Les étapes suivantes marqueront le développement de ce portail'
+      imageLink='/images/illustrations/progress_illustration.png'
+      id='feuille-de-route'
+    >
+      <ul className='fr-text--sm dev-list'>
+        <li>
+          <span className='fr-icon-git-branch-fill' /> Mise en ligne d’une cartographie des projets et d’un annuaire des APLC complétés de manière collaborative.
+        </li>
+        <li>
+          <span className='fr-icon-git-branch-fill' /> Consolidation de la documentation en continu
+        </li>
+        <li>
+          <span className='fr-icon-git-branch-fill' /> Construction d’un cadre de financement commun
+        </li>
+        <li>
+          <span className='fr-icon-git-branch-fill' /> Mise en valeur des PCRS d’ores et déjà disponibles sous licence ouverte.
+        </li>
+      </ul>
+
+      <style jsx>{`
+        .dev-list {
+          text-align: left;
+          display: grid;
+          justify-content: center;
+          gap: 10px;
+        }
+      `}</style>
+    </SectionImage>
+
+    <Section
+      title='Événements autour du PCRS'
+      background='blue'
+      id='evenements'
+      style={{
+        textAlign: 'center'
+      }}
+    >
+      <Image
+        src='/images/illustrations/calendar_illustration.png'
+        height={250}
+        width={250}
+        alt=''
+        className='fr-my-5w'
+      />
+      <div className='fr-p-2w fr-p-lg-4w'>
+        <p className='fr-py-1w'>
+          Tenez-vous informé des prochains événements organisés par l’ANCT à propos du PCRS. Ateliers, conférences, annonces pour être toujours à jour.
+        </p>
+        <Button
+          isWhite
+          href='/evenements'
+          buttonStyle='secondary'
+          label='Évenement à venir'
+        >
+          Événements à venir
+        </Button>
+      </div>
+    </Section>
+
+    <Section
+      title='Contactez-nous'
+      subtitle='Vous ne trouvez pas les réponses à vos questions sur ce site ou dans la documentation ?'
+      id='contact'
+      style={{
+        textAlign: 'center'
+      }}
+    >
+      <>
         <Image
-          src='/images/illustrations/calendar_illustration.png'
+          src='/images/illustrations/contact_illustration.png'
           height={250}
           width={250}
           alt=''
           className='fr-my-5w'
         />
-        <div className='fr-p-2w fr-p-lg-4w'>
-          <p className='fr-py-1w'>
-            Tenez-vous informé des prochains événements organisés par l’ANCT à propos du PCRS. Ateliers, conférences, annonces pour être toujours à jour.
+        <div className='fr-px-2w fr-px-lg-5w'>
+          <p className='fr-mb-1w fr-h5' style={{fontWeight: 'bold'}}>
+            Vous pouvez nous contacter !
           </p>
-          <Button
-            isWhite
-            href='/evenements'
-            buttonStyle='secondary'
-            label='Évenement à venir'
-          >
-            Événements à venir
-          </Button>
         </div>
-      </Section>
+      </>
+      <div className='separator fr-mt-3w fr-mb-5w' />
 
-      <Section
-        title='Contactez-nous'
-        subtitle='Vous ne trouvez pas les réponses à vos questions sur ce site ou dans la documentation ?'
-        id='contact'
-        style={{
-          textAlign: 'center'
-        }}
-      >
-        <>
-          <Image
-            src='/images/illustrations/contact_illustration.png'
-            height={250}
-            width={250}
-            alt=''
-            className='fr-my-5w'
-          />
-          <div className='fr-px-2w fr-px-lg-5w'>
-            <p className='fr-mb-1w fr-h5' style={{fontWeight: 'bold'}}>
-              Vous pouvez nous contacter !
-            </p>
+      <div className='fr-container--fluid'>
+        <div className=' fr-grid-row'>
+          <div className='communication-system fr-col-12 fr-col-md-6 fr-col-lg-5 fr-p-2w'>
+            <h3 className='fr-h6'>En rejoignant notre forum</h3>
+            <p>Un lieu d’échange entre <b>porteurs de projets</b>, <b>animateurs</b> et <b>utilisateurs des livrables</b> du PCRS. Venez nous rencontrer pour présenter <b>vos problématiques</b> et vous inspirer d’autres solutions dans les échanges existants</p>
+            <Button
+              isExternal
+              href='https://forum.pcrs.beta.gouv.fr/'
+              label='Rejoindre le forum'
+            >
+              <span className='fr-icon-question-answer-line' aria-hidden='true' />&nbsp;Rejoindre le forum
+            </Button>
           </div>
-        </>
-        <div className='separator fr-mt-3w fr-mb-5w' />
 
-        <div className='fr-container--fluid'>
-          <div className=' fr-grid-row'>
-            <div className='communication-system fr-col-12 fr-col-md-6 fr-col-lg-5 fr-p-2w'>
-              <h3 className='fr-h6'>En rejoignant notre forum</h3>
-              <p>Un lieu d’échange entre <b>porteurs de projets</b>, <b>animateurs</b> et <b>utilisateurs des livrables</b> du PCRS. Venez nous rencontrer pour présenter <b>vos problématiques</b> et vous inspirer d’autres solutions dans les échanges existants</p>
-              <Button
-                isExternal
-                href='https://forum.pcrs.beta.gouv.fr/'
-                label='Rejoindre le forum'
-              >
-                <span className='fr-icon-question-answer-line' aria-hidden='true' />&nbsp;Rejoindre le forum
-              </Button>
-            </div>
+          <div className='fr-col-lg-1 fr-col-xl-2' />
 
-            <div className='fr-col-lg-1 fr-col-xl-2' />
-
-            <div className='communication-system fr-col-12 fr-col-md-6 fr-col-lg-5 fr-mx-auto fr-p-2w'>
-              <h3 className='fr-h6'>En nous envoyant un email</h3>
-              <p>Notre équipe fera le nécessaire pour vous répondre dans les plus brefs délais, dans la limite de sa disponibilité.</p>
-              <Button
-                isExternal
-                href='mailto:contact@pcrs.beta.gouv.fr'
-                label='Contacter l’équipe'
-              >
-                <span className='fr-icon-mail-line' aria-hidden='true' />&nbsp;Contactez-nous
-              </Button>
-            </div>
+          <div className='communication-system fr-col-12 fr-col-md-6 fr-col-lg-5 fr-mx-auto fr-p-2w'>
+            <h3 className='fr-h6'>En nous envoyant un email</h3>
+            <p>Notre équipe fera le nécessaire pour vous répondre dans les plus brefs délais, dans la limite de sa disponibilité.</p>
+            <Button
+              isExternal
+              href='mailto:contact@pcrs.beta.gouv.fr'
+              label='Contacter l’équipe'
+            >
+              <span className='fr-icon-mail-line' aria-hidden='true' />&nbsp;Contactez-nous
+            </Button>
           </div>
         </div>
+      </div>
 
-        <style jsx>{`
-          .separator {
-            width: 100%;
-            border: solid 1px ${colors.blueFrance925};
-          }
+      <style jsx>{`
+        .separator {
+          width: 100%;
+          border: solid 1px ${colors.blueFrance925};
+        }
 
-          .communication-systems p {
-            text-align: left;
-          }
+        .communication-systems p {
+          text-align: left;
+        }
 
-          .communication-system {
-            min-width: 300px;
-            display: grid;
-            grid-template-rows: auto 1fr auto;
-            justify-items: center;
-          }
-        `}</style>
-      </Section>
+        .communication-system {
+          min-width: 300px;
+          display: grid;
+          grid-template-rows: auto 1fr auto;
+          justify-items: center;
+        }
+      `}</style>
+    </Section>
 
-      <Section
-        title='Suivez l’actualité'
-        subtitle='En vous inscrivant à la newsletter ou en nous suivant sur Twitter'
-        background='secondary'
-        style={{
-          textAlign: 'center'
-        }}
-      >
-        <Follow />
-      </Section>
-    </Page>
-  )
-}
+    <Section
+      title='Suivez l’actualité'
+      subtitle='En vous inscrivant à la newsletter ou en nous suivant sur Twitter'
+      background='secondary'
+      style={{
+        textAlign: 'center'
+      }}
+    >
+      <Follow />
+    </Section>
+  </Page>
+)
 
 export default Home
