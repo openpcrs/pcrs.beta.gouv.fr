6666233203f772c495f6bd0c82e33c45cf85f9b2
Guillaume <guillaume@livingdata.co>
2023-02-01 10:12:54 +0100

    Add Joi validation & display better errors

--- scripts/build-data.js
+++ scripts/build-data.js
@@ -1,57 +1,93 @@
 #!/usr/bin/env node
 /* eslint-disable no-await-in-loop */
+/* eslint-disable camelcase */
 
 import process from 'node:process'
 import path from 'node:path'
 import {readdir, readFile, writeFile} from 'node:fs/promises'
 import yaml from 'js-yaml'
+import Joi from 'joi'
 import {createGeometryBuilder} from '../lib/build-geometry.js'
 
 const pcrsFolder = './data'
 
-const REQUIRED_FIELDS = {
-  keys: [
-    'nom',
-    'regime',
-    'nature',
-    'livrables',
-    'acteurs',
-    'perimetres',
-    'etapes',
-    'subventions'
-  ],
-  etapes: [
-    'statut',
-    'date_debut'
-  ],
-  livrables: [
-    'nom',
-    'nature',
-    'licence'
-  ],
-  acteurs: [
-    'siren'
-  ]
-}
+const acteursSchema = Joi.object().keys({
+  siren: Joi.number().required(),
+  nom: Joi.string(),
+  interlocuteur: Joi.string(),
+  mail: Joi.string().email(),
+  telephone: Joi.string().pattern(/^((\+)33|0|0033)[1-9](\d{2}){4}$/),
+  role: Joi.valid(
+    'aplc',
+    'financeur',
+    'diffuseur',
+    'presta_vol',
+    'presta_lidar',
+    'controleur'
+  ),
+  finance_part_perc: Joi.number(),
+  finance_part_euro: Joi.number()
+})
 
-function validateField(entry, requiredFields) {
-  if (requiredFields.some(field => !Object.keys(entry).includes(field))) {
-    throw new Error('Tous les champs obligatoires ne sont pas présent dans ce fichier')
-  }
-}
+const etapesSchema = Joi.object().keys({
+  statut: Joi.valid(
+    'investigation',
+    'production',
+    'produit',
+    'livre',
+    'obsolete'
+  ).required(),
+  date_debut: Joi.date().allow(null)
+})
 
-function validateFields(fields, requiredFields) {
-  for (const key of fields) {
-    validateField(key, requiredFields)
-  }
-}
+const livrablesSchema = Joi.object().keys({
+  nom: Joi.string().required(),
+  nature: Joi.valid(
+    'geotiff',
+    'jpeg2000',
+    'gml'
+  ).required(),
+  licence: Joi.valid(
+    'ouverte_lo',
+    'ouvert_odbl',
+    'ferme'
+  ).required(),
+  diffusion: Joi.valid(
+    'telechargement',
+    'flux'
+  ),
+  crs: Joi.string(),
+  avancement: Joi.string(),
+  compression: Joi.string()
+})
 
-function validateProjetPCRS(entry) {
-  validateField(entry, REQUIRED_FIELDS.keys)
-  validateFields(entry.etapes, REQUIRED_FIELDS.etapes)
-  validateFields(entry.livrables, REQUIRED_FIELDS.livrables)
-  validateFields(entry.acteurs, REQUIRED_FIELDS.acteurs)
-}
+const subventionsSchema = Joi.object().keys({
+  nom: Joi.string().required(),
+  nature: Joi.valid(
+    'feder',
+    'cepr'
+  ).required()
+})
+
+const schema = Joi.object({
+  nom: Joi.string()
+    .min(3)
+    .required(),
+  regime: Joi.valid(
+    'production',
+    'maj'
+  ).required(),
+  nature: Joi.valid(
+    'vecteur',
+    'raster',
+    'mixte'
+  ).required(),
+  livrables: Joi.array().items(livrablesSchema).required(),
+  acteurs: Joi.array().items(acteursSchema).required(),
+  perimetres: Joi.array().required(),
+  etapes: Joi.array().items(etapesSchema).required(),
+  subventions: Joi.array().items(subventionsSchema).required().allow(null)
+}).prefs({convert: false})
 
 const geometryBuilder = await createGeometryBuilder()
 
@@ -67,7 +103,11 @@ async function buildPCRSData() {
       const filePath = path.join(pcrsFolder, fileName)
       const projet = yaml.load(await readFile(filePath))
 
-      validateProjetPCRS(projet)
+      try {
+        await schema.validateAsync(projet)
+      } catch (error) {
+        throw new Error(error)
+      }
 
       projet.id = fileName.slice(0, -5)
       projet.statut = (projet.etapes[projet.etapes.length - 1].statut === 'livre' ? 'livré' : projet.etapes[projet.etapes.length - 1].statut)
