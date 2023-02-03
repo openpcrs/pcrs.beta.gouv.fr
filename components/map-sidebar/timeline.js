cbfc2d67a73ab2defb539458f8727b2a3e2c7a50
Marine Le Pennec <lepennec.marine@gmail.com>
2023-02-03 16:29:50 +0100

    Improve label processing system

--- components/map-sidebar/timeline.js
+++ components/map-sidebar/timeline.js
@@ -9,39 +9,38 @@ import Tooltip from '@/components/tooltip.js'
 import Loader from '@/components/loader.js'
 
 const TIMELINE = [
-  {step: 1, label: 'investigation', isProgressingStep: true},
-  {step: 2, label: 'production', isProgressingStep: true},
-  {step: 3, label: 'produit'},
-  {step: 4, label: 'livre'}
+  {step: 1, value: 'investigation', label: 'Investigation', isProgressingStep: true},
+  {step: 2, value: 'production', label: 'Production', isProgressingStep: true},
+  {step: 3, value: 'produit', label: 'Produit'},
+  {step: 4, value: 'livre', label: 'Livré'}
 ]
 
 const Timeline = ({currentStatus, stepsColors, steps, isObsolete}) => {
   const timelineLength = TIMELINE.length
-  const currentStep = find(TIMELINE, step => step.label.toLowerCase() === currentStatus)
+  const currentStep = find(TIMELINE, step => step.value === currentStatus)
   const currentIndex = indexOf(TIMELINE, currentStep)
 
   return (
     <div className='fr-mb-3w'>
       <div className='timeline fr-mt-3w'>
         {Object.keys(TIMELINE).map((step, idx) => {
-          const {label} = TIMELINE[step]
-          const stepStartingDate = idx + 1 <= currentStep.step && formatDate(find(steps, {statut: TIMELINE[step].label.toLowerCase()}).date_debut)
+          const {label, value} = TIMELINE[step]
 
+          const isStepActualOrPassed = idx + 1 <= currentStep.step
           const isCurrentStep = currentStep.step === idx + 1
           const isProgressing = isCurrentStep && TIMELINE[step].isProgressingStep
 
-          const tooltipContent = () => {
-            const capitilizedLabelFirstLetter = TIMELINE[step].label.charAt(0).toUpperCase() + TIMELINE[step].label.slice(1)
+          const stepStartingDate = isStepActualOrPassed && formatDate(find(steps, {statut: TIMELINE[step].value}).date_debut)
 
-            return (
-              <>
-                <div className={`tooltip-label ${idx + 1 > currentStep.step ? 'futur-label' : ''}`}>
-                  {`${capitilizedLabelFirstLetter} ${isProgressing ? 'en cours' : ''}`}
-                </div>
-                {stepStartingDate && <div className='start-date fr-text--sm fr-mb-0'>Depuis le {stepStartingDate}</div>}
-              </>
-            )
-          }
+          const tooltipContent = () => (
+            <>
+              <div className={`tooltip-label ${idx + 1 > currentStep.step ? 'futur-label' : ''}`}>
+                {`${label} ${isProgressing ? 'en cours' : ''}`}
+              </div>
+
+              {stepStartingDate && <div className='start-date fr-text--sm fr-mb-0'>Depuis le {stepStartingDate}</div>}
+            </>
+          )
 
           // Last step
           if (idx === timelineLength - 1) {
@@ -54,7 +53,7 @@ const Timeline = ({currentStatus, stepsColors, steps, isObsolete}) => {
                 <div
                   className='circle'
                   style={{
-                    backgroundColor: isCurrentStep ? stepsColors[label] : colors.grey900,
+                    backgroundColor: isCurrentStep ? stepsColors[value] : colors.grey900,
                     color: isCurrentStep ? 'white' : colors.grey200
                   }}
                 >
@@ -77,7 +76,7 @@ const Timeline = ({currentStatus, stepsColors, steps, isObsolete}) => {
                   <div
                     className='circle'
                     style={{
-                      background: stepsColors[label],
+                      background: stepsColors[value],
                       color: 'black'
                     }}
                   >
@@ -95,7 +94,7 @@ const Timeline = ({currentStatus, stepsColors, steps, isObsolete}) => {
       {currentStep.step < timelineLength && (
         <div className='fr-text--sm'>
           Prochaine étape: <span className='next-step'>
-            {TIMELINE[currentIndex + 1].label === 'livre' ? 'livré' : TIMELINE[currentIndex + 1].label}
+            {TIMELINE[currentIndex + 1].value === 'livre' ? 'livré' : TIMELINE[currentIndex + 1].value}
           </span>
         </div>
       )}
