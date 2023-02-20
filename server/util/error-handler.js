07075ef54c8f4852208899b83a81cf5f7f688fe8
Guillaume <guillaume@livingdata.co>
2023-02-20 15:00:18 +0100

    Add middleware

---
+++ server/util/error-handler.js
@@ -0,0 +1,20 @@
+function errorHandler(err, req, res, _next) {
+  if (err) {
+    const statusCode = err.statusCode || 500
+    const exposeError = statusCode !== 500
+
+    res
+      .status(statusCode)
+      .send({
+        code: statusCode,
+        message: exposeError ? err.message : 'Une erreur inattendue est survenue.'
+      })
+
+    if (statusCode === 500) {
+      console.log(err)
+    }
+  }
+}
+
+export default errorHandler
+
---
+++ server/util/mongo.js
@@ -0,0 +1,44 @@
+import {MongoClient, ObjectId} from 'mongodb'
+
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost'
+const MONGODB_DBNAME = process.env.MONGODB_DBNAME || 'pcrs'
+
+class Mongo {
+  async connect(connectionString) {
+    if (this.db) {
+      throw new Error('mongo.connect() should not be called twice')
+    }
+
+    this.client = new MongoClient(connectionString || MONGODB_URL)
+    await this.client.connect()
+    this.dbName = MONGODB_DBNAME
+    this.db = this.client.db(this.dbName)
+
+    await this.createIndexes()
+  }
+
+  async createIndexes() {
+    await this.db.collection('projets').createIndex({nom: 1}, {unique: true})
+  }
+
+  disconnect(force) {
+    const {client} = this
+    this.client = undefined
+    this.db = undefined
+
+    return client.close(force)
+  }
+
+  parseObjectId(string) {
+    try {
+      return new ObjectId(string)
+    } catch {
+      return null
+    }
+  }
+}
+
+const mongo = new Mongo()
+
+export default mongo
+export {ObjectId} from 'mongodb'
