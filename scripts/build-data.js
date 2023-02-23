dd025530c457210d39afb2496aa8e4dab322db29
Guillaume <guillaume@livingdata.co>
2023-02-23 15:31:19 +0100

    Update validation: Allow APLC = null

--- scripts/build-data.js
+++ scripts/build-data.js
@@ -33,7 +33,7 @@ async function buildPCRSData() {
       projet.id = fileName.slice(0, -5)
       projet.statut = projet.etapes[projet.etapes.length - 1].statut
       projet.dateStatut = projet.etapes[projet.etapes.length - 1].date_debut
-      projet.aplc = projet.acteurs.find(acteur => acteur.role === 'aplc').nom
+      projet.aplc = projet.acteurs.find(acteur => acteur.role === 'aplc')?.nom || null
       projet.territoires = geometryBuilder.getTerritoriesName(projet.perimetres)
 
       projets.push(projet)
