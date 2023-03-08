f735a39ca0bdb6dc5de91e1680f331e9e6a1bc64
Guillaume <guillaume@livingdata.co>
2023-03-08 18:20:34 +0100

    Add minimal authentification

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import process from 'node:process'
 
+import * as dotenv from 'dotenv'
+
 import express from 'express'
 import createError from 'http-errors'
 import next from 'next'
@@ -10,8 +12,11 @@ import w from './util/w.js'
 
 import {getProjet, getProjets, createProjet, deleteProjet, updateProjet} from './projets.js'
 
+dotenv.config()
+
 const port = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== 'production'
+const {ADMIN_TOKEN} = process.env
 
 const server = express()
 const nextApp = next({dev})
@@ -21,6 +26,22 @@ await nextApp.prepare()
 await mongo.connect()
 
 server.use(express.json())
+
+if (!ADMIN_TOKEN) {
+  throw new Error('Le serveur ne peut pas démarrer car ADMIN_TOKEN n\'est pas défini')
+}
+
+function ensureAdmin(req, res, next) {
+  if (!req.get('Authorization') || !req.get('Authorization').startsWith('Token ')) {
+    throw createError(401, 'Cette action nécessite une authentification')
+  }
+
+  if (req.get('Authorization').slice(6) !== ADMIN_TOKEN) {
+    throw createError(403, 'Authentification refusée')
+  }
+
+  next()
+}
 
 server.param('projetId', w(async (req, res, next) => {
   const {projetId} = req.params
@@ -38,11 +59,11 @@ server.route('/projets/:projetId')
   .get(w(async (req, res) => {
     res.send(req.projet)
   }))
-  .delete(w(async (req, res) => {
+  .delete(w(ensureAdmin), w(async (req, res) => {
     await deleteProjet(req.projet._id)
     res.sendStatus(204)
   }))
-  .put(w(async (req, res) => {
+  .put(w(ensureAdmin), w(async (req, res) => {
     const projet = await updateProjet(req.projet._id, req.body)
     res.send(projet)
   }))
@@ -52,7 +73,7 @@ server.route('/projets')
     const projets = await getProjets()
     res.send(projets)
   }))
-  .post(w(async (req, res) => {
+  .post(w(ensureAdmin), w(async (req, res) => {
     const projet = await createProjet(req.body)
     res.status(201).send(projet)
   }))
