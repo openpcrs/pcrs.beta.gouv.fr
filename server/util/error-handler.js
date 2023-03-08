325c1e65d03a1cda1321144a6894755273cc51c9
Guillaume <guillaume@livingdata.co>
2023-03-08 15:55:20 +0100

    Add middlewares

---
+++ server/util/error-handler.js
@@ -0,0 +1,21 @@
+function errorHandler(err, req, res, _next) {
+  if (err) {
+    const statusCode = err.statusCode || 500
+    const exposeError = statusCode !== 500
+
+    res
+      .status(statusCode)
+      .send({
+        code: statusCode,
+        message: exposeError ? err.message : 'Une erreur inattendue est survenue',
+        validationErrors: err.details
+      })
+
+    if (statusCode === 500) {
+      console.log(err)
+    }
+  }
+}
+
+export default errorHandler
+
