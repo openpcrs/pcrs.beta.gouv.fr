71e856700b4b53c936fa58c9cdb4979cf9698876
Guillaume <guillaume@livingdata.co>
2023-03-13 10:38:53 +0100

    Tests: clean database between test & check if projet is in database

--- server/__tests__/projets.js
+++ server/__tests__/projets.js
@@ -19,59 +19,69 @@ test.after.always('cleanup', async () => {
   await mongod.stop()
 })
 
-test.serial('create valid projet', async t => {
-  const projet = await createProjet({
-    nom: 'Nom du pcrs',
-    regime: 'production',
-    nature: 'raster',
-    livrables: [
-      {
-        nom: 'Nom du livrable',
-        nature: 'geotiff',
-        diffusion: 'telechargement',
-        licence: 'ouvert_odbl',
-        avancement: '100',
-        crs: 'EPSG:2971',
-        compression: 'Nature untelle'
-      }
-    ],
-    acteurs: [
-      {
-        nom: 'living data',
-        siren: 813600889,
-        role: 'aplc',
-        telephone: '0600000000',
-        finance_part_perc: 50,
-        finance_part_euro: 120000
-      }
-    ],
-    perimetres: [
-      'epci:200039865'
-    ],
-    etapes: [
-      {
-        statut: 'investigation',
-        date_debut: '2023-02-06'
-      },
-      {
-        statut: 'production',
-        date_debut: null
-      }
-    ],
-    subventions: [
-      {
-        nom: 'Une subvention voilà quoi',
-        nature: 'feder',
-        montant: 100000,
-        echeance: '2024-04-10'
-      }
-    ]
-  })
+test.beforeEach('Clean database', async () => {
+  await mongo.db.collection('projets').deleteMany()
+})
 
-  t.true(requiredKeys.every(k => k in projet))
-  t.is(projet.nom, 'Nom du pcrs')
-  t.is(projet.regime, 'production')
-  t.is(projet.etapes.length, 2)
+const validProjet = {
+  nom: 'Nom du pcrs',
+  regime: 'production',
+  nature: 'raster',
+  livrables: [
+    {
+      nom: 'Nom du livrable',
+      nature: 'geotiff',
+      diffusion: 'telechargement',
+      licence: 'ouvert_odbl',
+      avancement: '100',
+      crs: 'EPSG:2971',
+      compression: 'Nature untelle'
+    }
+  ],
+  acteurs: [
+    {
+      nom: 'living data',
+      siren: 813600889,
+      role: 'aplc',
+      telephone: '0600000000',
+      finance_part_perc: 50,
+      finance_part_euro: 120000
+    }
+  ],
+  perimetres: [
+    'epci:200039865'
+  ],
+  etapes: [
+    {
+      statut: 'investigation',
+      date_debut: '2023-02-06'
+    },
+    {
+      statut: 'production',
+      date_debut: null
+    }
+  ],
+  subventions: [
+    {
+      nom: 'Une subvention voilà quoi',
+      nature: 'feder',
+      montant: 100000,
+      echeance: '2024-04-10'
+    }
+  ]
+}
+
+test.serial('create valid projet', async t => {
+  const projet = await createProjet(validProjet)
+
+  const createdProjet = await mongo.db.collection('projets').findOne({nom: 'Nom du pcrs'})
+
+  t.true(requiredKeys.every(k => k in createdProjet))
+  t.is(createdProjet.nom, 'Nom du pcrs')
+  t.is(createdProjet.regime, 'production')
+  t.is(createdProjet.etapes.length, 2)
+  t.is(projet.nom, createdProjet.nom)
+  t.is(createdProjet.regime, projet.regime)
 })
 
 test.serial('create invalid projet', async t => {
@@ -85,69 +95,28 @@ test.serial('create invalid projet', async t => {
 })
 
 test.serial('update projet', async t => {
-  const projet = await createProjet({
-    nom: 'Nom du pcrs à mettre à jour',
-    regime: 'production',
-    nature: 'raster',
-    livrables: [
-      {
-        nom: 'Nom du livrable',
-        nature: 'geotiff',
-        diffusion: 'telechargement',
-        licence: 'ouvert_odbl',
-        avancement: '100',
-        crs: 'EPSG:2971',
-        compression: 'Nature untelle'
-      }
-    ],
-    acteurs: [
-      {
-        nom: 'living data',
-        siren: 813600889,
-        role: 'aplc',
-        telephone: '0600000000',
-        finance_part_perc: 50,
-        finance_part_euro: 120000
-      }
-    ],
-    perimetres: [
-      'epci:200039865'
-    ],
-    etapes: [
-      {
-        statut: 'investigation',
-        date_debut: '2023-02-06'
-      },
-      {
-        statut: 'production',
-        date_debut: null
-      }
-    ],
-    subventions: [
-      {
-        nom: 'Une subvention voilà quoi',
-        nature: 'feder',
-        montant: 100000,
-        echeance: '2024-04-10'
-      }
-    ]
-  })
+  const projet = await createProjet(validProjet)
 
-  const updatedProjet = await updateProjet(projet._id, {nom: 'plop'})
+  const update = await updateProjet(projet._id, {nom: 'plop'})
+  const updatedProjet = await mongo.db.collection('projets').findOne({nom: 'plop'})
 
   t.is(updatedProjet.nom, 'plop')
+  t.is(update.nom, updatedProjet.nom)
 })
 
 test.serial('get all projets', async t => {
+  await createProjet(validProjet)
   const projets = await getProjets()
 
-  t.is(projets.length, 2)
+  t.is(projets.length, 1)
 })
 
 test.serial('delete projet', async t => {
-  const projet = await mongo.db.collection('projets').findOne({nom: 'plop'})
+  const projet = await createProjet(validProjet)
   const {deletedCount} = await deleteProjet(projet._id)
+  const deletedProjet = await mongo.db.collection('projets').findOne({nom: 'plop'})
 
   t.is(deletedCount, 1)
+  t.is(deletedProjet, null)
 })
 
