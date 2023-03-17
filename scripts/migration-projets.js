84f878099082a99dae0fe4d3a25e869306141f12
Guillaume <guillaume@livingdata.co>
2023-03-17 15:32:49 +0100

    Add migration script (/data -> bdd)

---
+++ scripts/migration-projets.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+/* eslint-disable no-await-in-loop */
+
+import process from 'node:process'
+import path from 'node:path'
+import {readdir, readFile} from 'node:fs/promises'
+import yaml, {JSON_SCHEMA} from 'js-yaml'
+import mongo from '../server/util/mongo.js'
+import {createProjet} from '../server/projets.js'
+
+async function projetsMigration() {
+  const filesList = await readdir('./data')
+
+  await mongo.connect()
+
+  for (const fileName of filesList) {
+    if (fileName.endsWith('.yaml') && fileName !== 'projet-exemple.yaml') {
+      const filePath = path.join('./data', fileName)
+      const projet = yaml.load(await readFile(filePath), {schema: JSON_SCHEMA})
+
+      console.log('Insertion du projet ->', projet.nom)
+
+      await createProjet(projet)
+    }
+  }
+
+  console.log('\nLes projets ont bien été importés dans la base 🎉\n')
+
+  mongo.disconnect()
+}
+
+projetsMigration().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
+
