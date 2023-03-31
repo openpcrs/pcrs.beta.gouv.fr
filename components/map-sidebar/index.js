865c773fdd4780ea2f04481c2cfe510f57bbb81f
Guillaume <guillaume@livingdata.co>
2023-03-31 11:20:44 +0200

    Fix APLC null

--- components/map-sidebar/index.js
+++ components/map-sidebar/index.js
@@ -15,7 +15,7 @@ const MapSidebar = ({projet, onClose}) => {
   const {status} = PCRS_DATA_COLORS
   const {nom, territoires, _id, etapes, source, documentation, contrat, acteurs} = projet
 
-  const contactAPLC = acteurs.find(acteur => acteur.role === 'aplc')
+  const contactAPLC = acteurs.find(acteur => acteur.role === 'aplc') || null
   const {statut} = projet.etapes[projet.etapes.length - 1]
   const projectStartDate = formatDate(find(projet.etapes, {statut: 'investigation'}).date_debut)
   const isObsolete = statut === 'obsolete'
