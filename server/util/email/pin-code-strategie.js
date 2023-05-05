7b2eec51b592be138c63d8db2a74127c52ea8189
Guillaume <guillaume@livingdata.co>
2023-05-05 11:28:01 +0200

    Add sendPinCodeMail strategie

---
+++ server/util/email/pin-code-strategie.js
@@ -0,0 +1,31 @@
+import {deburr} from 'lodash-es'
+import randomNumber from 'random-number-csprng'
+
+import {sendMail} from '../sendmail.js'
+import {formatMail} from './pin-code-template.js'
+
+export function normalize(string) {
+  return deburr(string).toLowerCase()
+}
+
+export function validateEmail(email) {
+  const regex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[(?:\d{1,3}\.){3}\d{1,3}])|(([a-zA-Z\-\d]+\.)+[a-zA-Z]{2,}))$/
+  return regex.test(String(email).toLowerCase())
+}
+
+export async function generatePinCode() {
+  const number = await randomNumber(0, 999_999)
+  return number.toString().padStart(6, '0')
+}
+
+export async function sendPinCodeMail(email) {
+  if (validateEmail(normalize(email))) {
+    const pinCode = await generatePinCode()
+    const emailContent = formatMail({pinCode, userEmail: email})
+
+    await sendMail(emailContent, email)
+  } else {
+    throw new Error('Cette adresse courriel est invalide')
+  }
+}
+
