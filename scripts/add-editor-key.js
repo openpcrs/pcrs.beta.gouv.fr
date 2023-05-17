2406d6a28e26106fc6e995ad69ae8fe318191b21
Guillaume <guillaume@livingdata.co>
2023-05-17 09:52:54 +0200

    Remove process.env

--- scripts/add-editor-key.js
+++ scripts/add-editor-key.js
@@ -1,18 +1,8 @@
 #!/usr/bin/env node
-/* eslint-disable import/first, import/order */
 /* eslint-disable no-await-in-loop */
 
-import * as dotenv from 'dotenv'
-
-dotenv.config()
-
-import process from 'node:process'
 import mongo from '../server/util/mongo.js'
 import {generateEditorKey} from '../server/util/generate-key.js'
-
-if (process.env.RUN_MIGRATION_KEY !== '1') {
-  process.exit(0)
-}
 
 await mongo.connect()
 
