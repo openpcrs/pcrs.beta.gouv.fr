c99a018c3eac3140b5eeff130834ccec43da1010
Guillaume <guillaume@livingdata.co>
2023-05-17 10:41:30 +0200

    simplify generateEditorKey

--- server/projets.js
+++ server/projets.js
@@ -18,16 +18,10 @@ export function expandProjet(projet) {
 export async function generateEditorKey(projetId) {
   projetId = mongo.parseObjectId(projetId)
 
-  const projet = await mongo.db.collection('projets').findOne({_id: projetId})
-
-  if (!projet.editorKey) {
-    const nanoId = nanoid()
-
-    await mongo.db.collection('projets').updateOne(
-      {_id: projet._id},
-      {$set: {editorKey: nanoId}}
-    )
-  }
+  await mongo.db.collection('projets').updateOne(
+    {_id: projetId, editorKey: {$exists: false}},
+    {$set: {editorKey: nanoid()}}
+  )
 }
 
 export function checkIsEditor(projet, req) {
