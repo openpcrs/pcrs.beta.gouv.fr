b0a912ec46a5e8477ee77b15c0f300206edfa6a8
Marine Le Pennec <lepennec.marine@gmail.com>
2023-05-26 14:37:25 +0200

    Handle payload errors

---
+++ components/suivi-form/utils/payload-errors.js
@@ -0,0 +1,17 @@
+/* eslint-disable camelcase */
+
+export const PAYLOAD_ERRORS = {
+  // Général
+  nom: 'Un nom est manquant',
+  nature: 'La nature n’a pas été sélectionné',
+  // Livrables
+  licence: 'Un type de licence n’a pas été sélectionné',
+  avancement: 'Un pourcentage de progression de l’avancement suivant est invalide',
+  // Acteurs
+  mail: 'l’email suivant est invalide',
+  telephone: 'Le numéro de téléphone suivant est invalide',
+  finance_part_euro: 'La part de financement suivante est invalide',
+  finance_part_perc: 'Le montant du financement suivant est invalide',
+  siren: 'Un numéro SIREN est manquant ou invalide',
+  role: 'Un rôle n’a pas été sélectionnée'
+}
