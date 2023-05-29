df742887400fcc71d71ced0734fb3630b7ac4dbd
Marine Le Pennec <lepennec.marine@gmail.com>
2023-05-29 09:44:19 +0200

    Fix wording

--- components/suivi-form/utils/payload-errors.js
+++ components/suivi-form/utils/payload-errors.js
@@ -3,12 +3,12 @@
 export const PAYLOAD_ERRORS = {
   // Général
   nom: 'Un nom est manquant',
-  nature: 'La nature n’a pas été sélectionné',
+  nature: 'La nature n’a pas été sélectionnée',
   // Livrables
   licence: 'Un type de licence n’a pas été sélectionné',
-  avancement: 'Un pourcentage de progression de l’avancement suivant est invalide',
+  avancement: 'Un pourcentage de progression de l’avancement est invalide',
   // Acteurs
-  mail: 'l’email suivant est invalide',
+  mail: 'L’email suivant est invalide',
   telephone: 'Le numéro de téléphone suivant est invalide',
   finance_part_euro: 'La part de financement suivante est invalide',
   finance_part_perc: 'Le montant du financement suivant est invalide',
