05f1d2d4b0b9c26c7257c993b9fdbffe404392c2
Guillaume <guillaume@livingdata.co>
2023-06-02 10:27:50 +0200

    Add migration from external file

---
+++ scripts/authorized-creators-emails.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+/* eslint-disable no-await-in-loop */
+
+import mongo from '../server/util/mongo.js'
+import {getAuthorizedEmails, validateEmail} from '../server/auth/pin-code/index.js'
+
+await mongo.connect()
+
+const emails = await getAuthorizedEmails()
+
+for (const email of emails) {
+  if (validateEmail(email)) {
+    const creator = {email}
+
+    mongo.decorateCreation(creator)
+
+    await mongo.db.collection('creators-emails').updateOne({email: creator.email}, {$setOnInsert: {...creator}}, {upsert: true})
+  }
+}
+
+console.log('Terminé !')
+
+await mongo.disconnect()
