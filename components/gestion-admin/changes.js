e898fc373777b5bcd0d6f193208c3b0b97222de3
Guillaume <guillaume@livingdata.co>
2023-06-13 15:03:31 +0200

    Add filter

--- components/gestion-admin/changes.js
+++ components/gestion-admin/changes.js
@@ -6,8 +6,10 @@ import Loader from '../loader.js'
 
 const Changes = ({token}) => {
   const [changes, setChanges] = useState()
+  const [search, setSearch] = useState()
+  const [filteredChanges, setFilteredChanges] = useState()
 
-  const getChanges = useCallback(async () => {
+  const getAllChanges = useCallback(async () => {
     const response = await fetch('/report', {
       method: 'GET',
       headers: {
@@ -35,44 +37,66 @@ const Changes = ({token}) => {
   }
 
   useEffect(() => {
-    getChanges()
+    if (changes && search) {
+      setFilteredChanges(changes.filter(p => p.nom.includes(search)))
+    } else {
+      setFilteredChanges(changes)
+    }
+  }, [changes, search])
+
+  useEffect(() => {
+    getAllChanges()
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  if (changes?.length === 0) {
-    return <div><i>Aucun changement dans les dernières 24h</i></div>
-  }
-
   return (
     <div>
-      <div className='fr-table fr-table--bordered'>
-        <table>
-          <thead>
-            <tr>
-              <th scope='col' className='fr-p-3w'>Nom du projet</th>
-              <th scope='col' className='fr-p-3w'>Action</th>
-              <th scope='col' className='fr-p-3w'>Depuis</th>
-            </tr>
-          </thead>
-          {changes ? (
-            <tbody>
-              {changes.map(change => {
-                const lastChange = returnLastChange(change)
-                const modificationTime = new Date(change._updated)
+      <div className='fr-col-12 fr-col-md-4'>
+        <label className='fr-label fr-mb-1w'>
+          <b>Rechercher un projet :</b>
+        </label>
+        <div className='fr-search-bar' >
+          <input
+            className='fr-input'
+            placeholder='Chercher un projet'
+            type='search'
+            onChange={e => setSearch(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className='fr-table fr-table--layout-fixed'>
+        {filteredChanges ? (
+          <>
+            <table>
+              <thead>
+                <tr>
+                  <th scope='col' className='fr-p-2w'>Nom du projet</th>
+                  <th scope='col' className='fr-p-2w'>Action</th>
+                  <th scope='col' className='fr-p-2w'>Depuis</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredChanges.map(change => {
+                  const lastChange = returnLastChange(change)
+                  const modificationTime = new Date(change._updated)
 
-                return (
-                  <tr key={change.nom}>
-                    <td className='fr-p-3w'><b>{change?.nom}</b></td>
-                    <td className='fr-p-3w'>{lastChange}</td>
-                    <td className='fr-p-3w'><i>{`(Il y a ${formatDistanceToNow(modificationTime, {locale: fr})})`}</i></td>
-                  </tr>
-                )
-              })}
-            </tbody>
-          ) : (
-            <Loader />
-          )}
-        </table>
+                  return (
+                    <tr key={change.nom}>
+                      <td className='fr-p-2w'><b>{change?.nom}</b></td>
+                      <td className='fr-p-2w'>{lastChange}</td>
+                      <td className='fr-p-2w'><i>{`(Il y a ${formatDistanceToNow(modificationTime, {locale: fr})})`}</i></td>
+                    </tr>
+                  )
+                })}
+              </tbody>
+            </table>
+            {filteredChanges.length === 0 && (
+              <div className='fr-p-2w'><i>Aucun résultat...</i></div>
+            )}
+          </>
+        ) : (
+          <Loader />
+        )}
       </div>
     </div>
   )
