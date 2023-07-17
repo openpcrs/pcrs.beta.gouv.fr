6ad7e0ed8e41de6919692cb22b8518206c2deb7c
Marine Le Pennec <lepennec.marine@gmail.com>
2023-07-17 16:12:05 +0200

    Add a no library reusable autocomplete input

--- components/autocomplete-input.js
+++ components/autocomplete-input.js
@@ -1,6 +1,8 @@
-import {useCallback} from 'react'
+import {useState, useRef, useCallback} from 'react'
 import PropTypes from 'prop-types'
-import Autocomplete from 'react-autocomplete'
+import {uniqueId} from 'lodash'
+
+import colors from '@/styles/colors.js'
 
 import Loader from '@/components/loader.js'
 
@@ -11,91 +13,128 @@ const AutocompleteInput = ({
   placeholder,
   ariaLabel,
   errorMessage,
-  results,
-  customItem,
   isRequired,
   isDisabled,
-  isLoading,
-  onValueChange,
+  onInputChange,
   onSelectValue,
-  getItemValue
+  results,
+  renderItem,
+  isLoading
 }) => {
   const inputState = errorMessage ? 'error' : ''
+  const wrapperRef = useRef(null)
 
-  // Custom autocomplete input
-  const customInput = props => (
-    <div className={`fr-grid-row fr-search fr-input-group--${inputState}`} role='search'>
-      <label className='fr-col-12'>
-        <div className={isRequired ? 'required-label' : ''} >{label}</div>
+  const [isSuggestionsMenuOpen, setIsSuggestionsMenuOpen] = useState(false)
 
-        {description && <span className='fr-hint-text fr-mb-2w fr-mt-0'>{description}</span>}
-      </label>
+  const onValueChange = useCallback(e => {
+    onInputChange(e.target.value)
+    setIsSuggestionsMenuOpen(true)
+  }, [onInputChange])
 
-      <input
-        type='search'
-        aria-label={ariaLabel}
-        required={isRequired}
-        className={`fr-input fr-input--${inputState}`}
-        placeholder={placeholder}
-        disabled={isDisabled}
-        {...props}
-      />
+  const onLoseFocus = () => {
+    // Delay closing the suggestions menu to check if focus is on the list or input
+    setTimeout(() => {
+      if (!wrapperRef.current.contains(document.activeElement)) {
+        // Close the menu only if focus is not on the list or input
+        setIsSuggestionsMenuOpen(false)
+      }
+    }, 0)
+  }
 
-      {errorMessage && <p id='text-input-error-desc-error' className='fr-error-text'>{errorMessage}</p>}
+  const onFocus = () => {
+    if (results) {
+      setIsSuggestionsMenuOpen(true)
+    }
+  }
+
+  const onTabSelect = (event, item) => {
+    if (event.key === 'Enter') {
+      onSelectValue(item)
+      setIsSuggestionsMenuOpen(false)
+    }
+  }
+
+  return (
+    <div className='search-wrapper'>
+      <div className={`fr-grid-row fr-search fr-input-group--${inputState}`}>
+        <label className='fr-col-12'>
+          <div className={isRequired ? 'required-label' : ''} >{label}</div>
+
+          {description && <span className='fr-hint-text fr-mb-2w fr-mt-0'>{description}</span>}
+        </label>
+
+        <input
+          type='search'
+          aria-label={ariaLabel}
+          required={isRequired}
+          className={`fr-input fr-input--${inputState}`}
+          placeholder={placeholder}
+          disabled={isDisabled}
+          value={value}
+          onChange={onValueChange}
+          onFocus={onFocus}
+          onBlur={onLoseFocus}
+        />
+
+        {errorMessage && <p id='text-input-error-desc-error' className='fr-error-text'>{errorMessage}</p>}
+      </div>
+
+      {isSuggestionsMenuOpen && (
+        isLoading ? (
+          <div className='fr-grid-row fr-grid-row--center fr-p-2w fr-mt-1w fr-p-0 menu'><Loader size='small' /></div>
+        ) : (
+          <ul ref={wrapperRef} className='fr-mt-1w fr-p-0 menu' role='listbox'>
+            {results.map(item => (
+              <li
+                key={uniqueId()}
+                className='fr-p-2w'
+                tabIndex={0} // Allow keyboard focus
+                onClick={() => {
+                  setIsSuggestionsMenuOpen(false)
+                  onSelectValue(item)
+                }}
+                onKeyDown={e => onTabSelect(e, item)}
+              >
+                {renderItem(item)}
+              </li>
+            ))}
+          </ul>
+        )
+      )}
 
       <style jsx>{`
         .required-label::after {
           content: '*';
           margin-left: 5px
         }
-      `}</style>
-    </div>
-  )
 
-  // Custom suggestions dropdown menu
-  const customMenu = useCallback((items, value) => (
-    <div className={value.length > 0 ? 'menu fr-mt-1w' : 'hidden'}>
-      {isLoading && items.length === 0 ? (
-        <div className='fr-grid-row fr-grid-row--center fr-p-2w'><Loader size='small' /></div>
-      ) : (items.length === 0 ? (
-        <div className='item fr-p-2w'>Aucun résultat</div>
-      ) : items)}
-
-      <style jsx>{`
-        .menu {
-          position: absolute;
-          left: 0;
-          right: 0;
-          box-shadow: 2px 12px 23px 2px rgba(0,0,0,0.23);
-          border-radius: 0 0 5px 5px;
-          background: white;
+        .search-wrapper {
+          position: relative;
           z-index: 2;
         }
 
-        .hidden {
-          display: none;
+        .menu {
+          box-shadow: 2px 12px 23px 2px rgba(0,0,0,0.23);
+          border-radius: 0 0 5px 5px;
+          background: white;
+          position: absolute;
+          left: 0;
+          right: 0;
         }
-      `}</style>
-    </div>
-  ), [isLoading])
 
-  return (
-    <div className='search-input-wrapper'>
-      <Autocomplete
-        value={value}
-        getItemValue={getItemValue}
-        items={results}
-        renderItem={(item, isHighlighted) => customItem(item, isHighlighted)}
-        renderMenu={customMenu}
-        renderInput={customInput}
-        wrapperStyle={{width: '100%'}}
-        onChange={onValueChange}
-        onSelect={onSelectValue}
-      />
+        .menu li {
+          border-bottom: solid 2px ${colors.grey900};
+        }
 
-      <style jsx>{`
-        .search-input-wrapper {
-          position: relative;
+        .menu li:hover {
+          background-color: ${colors.info425};
+          color: white;
+          font-weight: bold;
+          cursor: pointer;
+        }
+
+        .menu li:last-child{
+          border-bottom: none;
         }
       `}</style>
     </div>
@@ -113,9 +152,8 @@ AutocompleteInput.propTypes = {
   isRequired: PropTypes.bool,
   isDisabled: PropTypes.bool,
   isLoading: PropTypes.bool.isRequired,
-  customItem: PropTypes.func.isRequired,
-  getItemValue: PropTypes.func.isRequired,
-  onValueChange: PropTypes.func.isRequired,
+  renderItem: PropTypes.func.isRequired,
+  onInputChange: PropTypes.func.isRequired,
   onSelectValue: PropTypes.func.isRequired
 }
 
