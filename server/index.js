036dd63de23c65b1b450278ca177355903c626c0
Guillaume <guillaume@livingdata.co>
2023-08-09 15:09:11 +0200

    Export administrators routes

--- server/index.js
+++ server/index.js
@@ -12,20 +12,19 @@ import process from 'node:process'
 import express from 'express'
 import next from 'next'
 import morgan from 'morgan'
-import createError from 'http-errors'
 
 import mongo from './util/mongo.js'
 import errorHandler from './util/error-handler.js'
 import w from './util/w.js'
 
-import {handleAuth, ensureAdmin, parseToken} from './auth/middleware.js'
+import {handleAuth} from './auth/middleware.js'
 
 import {getProjetsGeojson} from './projets.js'
 import dataRoutes from './routes/data.js'
 import projetsRoutes from './routes/projets.js'
 import authRoutes from './routes/auth.js'
 import creatorsEmailsRoutes from './routes/creators-emails.js'
-import {addAdministrator, getAdministrators, updateAdministrator, deleteAdministrator, getAdministratorById, isSelfDeleting} from './admin/administrators.js'
+import administratorsRoutes from './routes/administrators.js'
 
 const port = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -43,18 +42,6 @@ if (dev) {
   server.use(morgan('dev'))
 }
 
-server.param('adminId', w(async (req, res, next) => {
-  const {adminId} = req.params
-  const administrator = await getAdministratorById(adminId, req)
-
-  if (!administrator) {
-    throw createError(404, 'Cet administrateur n’existe pas')
-  }
-
-  req.administrator = administrator
-  next()
-}))
-
 // Pre-warm underlying cache
 await getProjetsGeojson()
 
@@ -63,38 +50,7 @@ server.use('/', dataRoutes)
 server.use('/', projetsRoutes)
 server.use('/', authRoutes)
 server.use('/', creatorsEmailsRoutes)
-
-server.route('/administrators/:adminId')
-  .all(w(ensureAdmin))
-  .get(w(async (req, res) => {
-    res.send(req.administrator)
-  }))
-  .delete(w(async (req, res) => {
-    const token = await parseToken(req)
-
-    await isSelfDeleting(req.params.adminId, token)
-    await deleteAdministrator(req.params.adminId)
-
-    res.sendStatus(204)
-  }))
-  .put(w(async (req, res) => {
-    const administrator = await updateAdministrator(req.params.adminId, req.body)
-
-    res.send(administrator)
-  }))
-
-server.route('/administrators')
-  .all(w(ensureAdmin))
-  .get(w(async (req, res) => {
-    const administrators = await getAdministrators()
-
-    res.send(administrators)
-  }))
-  .post(w(async (req, res) => {
-    const administrator = await addAdministrator(req.body)
-
-    res.send(administrator)
-  }))
+server.use('/administrators', administratorsRoutes)
 
 server.use(errorHandler)
 
