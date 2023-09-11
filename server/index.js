a3300e43862ef8ca4e633441bd3f25030b794ff2
Guillaume <guillaume@livingdata.co>
2023-09-11 11:25:52 +0200

    Add routes for calculator

--- server/index.js
+++ server/index.js
@@ -26,6 +26,7 @@ import authRoutes from './routes/auth.js'
 import creatorsEmailsRoutes from './routes/creators-emails.js'
 import administratorsRoutes from './routes/administrators.js'
 import reportRoutes from './routes/report.js'
+import calculatorRoutes from './routes/calculator.js'
 
 const port = process.env.PORT || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -53,6 +54,7 @@ server.use('/', authRoutes)
 server.use('/creators-emails', creatorsEmailsRoutes)
 server.use('/administrators', administratorsRoutes)
 server.use('/report', reportRoutes)
+server.use('/calculator', calculatorRoutes)
 
 server.use(errorHandler)
 
