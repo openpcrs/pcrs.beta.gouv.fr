9e96a33a0772758c71465c8fb76969efe8c75986
Guillaume <guillaume@livingdata.co>
2023-10-09 14:12:09 +0200

    Fix jump line

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 import {useContext} from 'react'
 import {useRouter} from 'next/router'
-
 import Link from 'next/link'
+
 import AuthentificationContext from '@/contexts/authentification-token.js'
 
 const Footer = () => {
