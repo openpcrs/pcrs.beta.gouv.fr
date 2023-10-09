db24cfab3c68e761ec6598352167edc587873f96
Marine Le Pennec <lepennec.marine@gmail.com>
2023-10-09 15:40:43 +0200

    Create <Tab /> component

---
+++ components/ui/tab.js
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types'
+
+const Tab = ({activeTab, tabs, icon, children, handleActiveTab}) => (
+  <div>
+    <div className='fr-tabs'>
+      <ul className='fr-tabs__list' role='tablist'>
+        {tabs.map((tab, index) => {
+          const defaultTab = !activeTab && index === 0
+
+          return (
+            <li key={tab.label} role='presentation'>
+              <button
+                disabled={tab?.isDisabled}
+                type='button'
+                className={`fr-tabs__tab ${icon ? `fr-icon-${icon}` : ''} fr-tabs__tab--icon-left`}
+                role='tab'
+                aria-selected={defaultTab || (activeTab === tab.value ? 'true' : 'false')}
+                onClick={() => handleActiveTab(tab.value)}
+              >
+                {tab.label}
+              </button>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+
+    <div className='content-wrapper fr-p-3w' role='tabpanel'>
+      {children}
+    </div>
+
+    <style jsx>{`
+      .content-wrapper {
+        border: 1px solid #dddddd;
+       }
+    `}</style>
+  </div>
+)
+
+Tab.propTypes = {
+  activeTab: PropTypes.string,
+  icon: PropTypes.string,
+  tabs: PropTypes.arrayOf(PropTypes.shape({
+    value: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    isDisabled: PropTypes.bool
+  })).isRequired,
+  handleActiveTab: PropTypes.func.isRequired,
+  children: PropTypes.node
+}
+
+Tab.defaultProps = {
+  activeTab: null,
+  icon: null,
+  children: null
+}
+
+export default Tab
