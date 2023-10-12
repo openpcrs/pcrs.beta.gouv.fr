34931095e75dbcd8e6d12407de3532d7defff43a
Théophile Merlière <theophile.merliere@gmail.com>
2023-10-12 23:32:38 +0200

    isURLValid only allow http URL

--- components/suivi-form/livrables/utils/url.js
+++ components/suivi-form/livrables/utils/url.js
@@ -1,4 +1,4 @@
 export function isURLValid(url) {
-  const pattern = /(\b(https?|ftp|file):\/\/[-\w+&@#/%?=~|!:,.;]*[-\w+&@#/%=~|])/i
+  const pattern = /\b(https?):\/\/[-\w+&@#/%?=~|!:,.;]*[-\w+&@#/%=~|]/i
   return pattern.test(url)
 }
