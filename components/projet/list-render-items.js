394b3cedeaf8c5a61afa93cbc3b2a63a0c1d71b1
Marine Le Pennec <lepennec.marine@gmail.com>
2023-10-13 15:42:26 +0200

    Improve drowpdown content design

--- components/projet/list-render-items.js
+++ components/projet/list-render-items.js
@@ -7,28 +7,27 @@ import ListItem from '@/components/projet/list-item.js'
 export const livrableRenderItem = livrable => (
   <ListItem title={livrable.nom}>
     <div className='content-wrapper fr-p-2w'>
-      <div>Nature : <span>{NATURES_LABELS[livrable.nature]}</span></div>
-      <div className='fr-mt-1w'>Licence : <span>{LICENCES_LABELS[livrable.licence]}</span></div>
-      <div className='fr-mt-1w'>Diffusion : <span>{DIFFUSIONS_LABELS[livrable.diffusion]}</span></div>
-      <div className='fr-mt-1w'>Type de publication : <span>{livrable.publication ? PUBLICATIONS_LABELS[livrable.publication] : 'Non renseigné'}</span></div>
-      <div className='fr-mt-1w'>Livraison : <span>{livrable.date_livraison ? `le ${formatDate(livrable.date_livraison)}` : 'Non renseigné'}</span></div>
+      <div className='fr-grid-row'><div className='title fr-mr-1w'>Nature : </div> <span>{NATURES_LABELS[livrable.nature]}</span></div>
+      <div className='fr-mt-1w fr-grid-row'><div className='title fr-mr-1w'>Licence :</div><span>{LICENCES_LABELS[livrable.licence]}</span></div>
+      <div className='fr-mt-1w fr-grid-row'><div className='title fr-mr-1w'>Diffusion :</div> <span>{DIFFUSIONS_LABELS[livrable.diffusion]}</span></div>
+      <div className='fr-mt-1w fr-grid-row'><div className='title fr-mr-1w'>Type de publication :</div><span>{livrable.publication ? PUBLICATIONS_LABELS[livrable.publication] : 'Non renseigné'}</span></div>
+      <div className='fr-mt-1w fr-grid-row'><div className='title fr-mr-1w'> Livraison :</div> <span>{livrable.date_livraison ? `le ${formatDate(livrable.date_livraison)}` : 'Non renseigné'}</span></div>
 
       <style jsx>{`
-      .content-wrapper {
-        background: ${colors.info425};
-        color: white;
-        border-radius: 5px;
-        text-align: left;
-        gap: 5px;
-      }
+        .content-wrapper {
+          background: white;
+          text-align: left;
+          gap: 5px;
+        }
 
-      .content-wrapper div {
-        font-weight: bold
-      }
+        .title {
+          color: ${colors.info425};
+          font-weight: bold;
+        }
 
-      .content-wrapper span {
-        font-weight: normal;
-      }
+        .content-wrapper span {
+          font-weight: normal;
+        }
     `}</style>
     </div>
   </ListItem>
@@ -37,29 +36,28 @@ export const livrableRenderItem = livrable => (
 export const acteurRenderItem = acteur => (
   <ListItem title={acteur.nom}>
     <div className='content-wrapper fr-p-2w'>
-      <div>SIREN : {acteur.siren}</div>
-      <div className='fr-mt-1w'>mail : {acteur.mail || 'Non renseigné'}</div>
-      <div className='fr-mt-1w'>Téléphone : {acteur.mail || 'Non renseigné'}</div>
-      <div className='fr-mt-1w'>Part de financement : {acteur.finance_part_perc || 'Non renseigné'}</div>
-      <div className='fr-mt-1w'>Montant du financement : {acteur.finance_part_euro || 'Non renseigné'}</div>
+      <div className='fr-grid-row'><div className='title fr-mr-1w'>SIREN :</div> {acteur.siren}</div>
+      <div className='fr-mt-1w fr-grid-row'><div className='title fr-mr-1w'>mail :</div> {acteur.mail || 'Non renseigné'}</div>
+      <div className='fr-mt-1w fr-grid-row'><div className='title fr-mr-1w'>Téléphone :</div> {acteur.mail || 'Non renseigné'}</div>
+      <div className='fr-mt-1w fr-grid-row'><div className='title fr-mr-1w'>Part de financement :</div> {acteur.finance_part_perc || 'Non renseigné'}</div>
+      <div className='fr-mt-1w fr-grid-row'><div className='title fr-mr-1w'>Montant du financement :</div> {acteur.finance_part_euro || 'Non renseigné'}</div>
 
       <style jsx>{`
-      .content-wrapper {
-        background: ${colors.info425};
-        color: white;
-        border-radius: 5px;
-        text-align: left;
-        gap: 5px;
-      }
+        .content-wrapper {
+          background: white;
+          text-align: left;
+          gap: 5px;
+        }
 
-      .content-wrapper div {
-        font-weight: bold
-      }
+        .title {
+          color: ${colors.info425};
+          font-weight: bold;
+        }
 
-      .content-wrapper span {
-        font-weight: normal;
-      }
-    `}</style>
+        .content-wrapper span {
+          font-weight: normal;
+        }
+      `}</style>
     </div>
   </ListItem>
 )
@@ -67,27 +65,26 @@ export const acteurRenderItem = acteur => (
 export const subventionRenderItem = subvention => (
   <ListItem title={subvention.nom}>
     <div className='content-wrapper fr-p-2w'>
-      <div>Nature : {SUBVENTIONS_NATURES_LABELS[subvention.nature]}</div>
-      <div className='fr-mt-1w'>Montant : <span>{`${subvention.montant ? `${subvention.montant}€` : 'Non renseigné'}`}</span></div>
-      <div className='fr-mt-1w'>Échance : {formatDate(subvention.echeance) || 'Non renseigné'}</div>
+      <div className='fr-grid-row'><div className='title fr-mr-1w'>Nature :</div> {SUBVENTIONS_NATURES_LABELS[subvention.nature]}</div>
+      <div className='fr-mt-1w fr-grid-row'><div className='title fr-mr-1w'>Montant :</div> <span>{`${subvention.montant ? `${subvention.montant}€` : 'Non renseigné'}`}</span></div>
+      <div className='fr-mt-1w fr-grid-row'><div className='title fr-mr-1w'>Échance :</div> {formatDate(subvention.echeance) || 'Non renseigné'}</div>
 
       <style jsx>{`
-      .content-wrapper {
-        background: ${colors.info425};
-        color: white;
-        border-radius: 5px;
-        text-align: left;
-        gap: 5px;
-      }
+        .content-wrapper {
+          background: white;
+          text-align: left;
+          gap: 5px;
+        }
 
-      .content-wrapper div {
-        font-weight: bold
-      }
+        .title {
+          color: ${colors.info425};
+          font-weight: bold;
+        }
 
-      .content-wrapper span {
-        font-weight: normal;
-      }
-    `}</style>
+        .content-wrapper span {
+          font-weight: normal;
+        }
+      `}</style>
     </div>
   </ListItem>
 )
