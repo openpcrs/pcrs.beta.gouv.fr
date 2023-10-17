84a7932e10b4e4bd17250d0a7e59de270498e203
Théophile Merlière <tmerlier@users.noreply.github.com>
2023-10-17 13:57:15 +0200

    Mise à jour du modèle en v0.4 [frontend] (#335)
    * Remove livrables publication
    
    * Remove livrable compression
    
    * Remove livrable crs
    
    * Reorganizes form style
    
    * Update projet status
    
    * Update obsolete color
    
    * Cleanup and update <Timeline>
    
    * Update map color
    
    * Update map popup status info
    
    * Update progress badge
    
    * Fix limit for adding step
    
    * Use STATUS as object
    
    * Update <Etapes/>

--- components/suivi-form/etapes.js
+++ components/suivi-form/etapes.js
@@ -1,8 +1,8 @@
 /* eslint-disable camelcase */
 import PropTypes from 'prop-types'
-import {findIndex} from 'lodash-es'
 
 import colors from '@/styles/colors.js'
+import {STATUS} from '@/lib/utils/projet.js'
 
 import {useInput} from '@/hooks/input.js'
 
@@ -10,42 +10,38 @@ import DateInput from '@/components/date-input.js'
 import TextInput from '@/components/text-input.js'
 import Button from '@/components/button.js'
 
-const STATUS = [
-  {label: 'Investigation', value: 'investigation'},
-  {label: 'Production', value: 'production'},
-  {label: 'Produit', value: 'produit'},
-  {label: 'Livré', value: 'livre'},
-  {label: 'Obsolète', value: 'obsolete'}
-]
-
 const Etapes = ({initialValue, etapes, handleEtapes}) => {
   const {statut, date_debut} = initialValue
 
   const [statutInput, setStatutInput] = useInput({initialValue: statut})
   const [startDate, setStartDate] = useInput({initialValue: date_debut})
 
-  const handleDateChange = (value, statut) => {
-    if (statut === statutInput) {
+  const statusKeys = Object.keys(STATUS)
+
+  const handleDateChange = event => {
+    const {value, name} = event.target
+
+    if (name === statutInput) {
       setStartDate(value)
     }
 
     handleEtapes(prevEtapes => {
       const etapesCopy = [...prevEtapes]
-      etapesCopy[etapesCopy.findIndex(e => e.statut === statut)] = {statut, date_debut: value}
+      etapesCopy[etapesCopy.findIndex(e => e.statut === name)] = {statut: name, date_debut: value}
       return etapesCopy
     })
   }
 
   const addStep = () => {
     if (startDate) {
-      const currentStepIndex = findIndex(STATUS, o => o.value === statutInput)
-      const nextStepIndex = findIndex(STATUS, o => o.value === statutInput) + 1
+      const currentStepIndex = statusKeys.indexOf(statutInput)
+      const nextStepIndex = statusKeys.indexOf(statutInput) + 1
 
       etapes[currentStepIndex].date_debut = startDate
 
-      handleEtapes([...etapes, {statut: STATUS[nextStepIndex].value, date_debut: ''}])
+      handleEtapes([...etapes, {statut: statusKeys[nextStepIndex], date_debut: ''}])
 
-      setStatutInput(STATUS[nextStepIndex].value)
+      setStatutInput(statusKeys[nextStepIndex])
       setStartDate('')
     }
   }
@@ -61,52 +57,49 @@ const Etapes = ({initialValue, etapes, handleEtapes}) => {
       <h3 className='fr-h5'>Étapes</h3>
       <hr className='separator fr-my-3w' />
 
-      {etapes.map((etape, index) => {
-        const findLabel = STATUS.find(status => etape.statut === status.value).label
-
-        return (
-          <div key={etape.statut} className='fr-grid-row fr-my-5w'>
-            <div className='fr-grid-row fr-col-11'>
-              <div className='fr-col-12 fr-col-md-6'>
-                <TextInput
-                  isRequired
-                  isDisabled
-                  id={etape.statut}
-                  label='Statut'
-                  ariaLabel='statut du projet'
-                  description='Statut du projet'
-                  value={findLabel}
-                />
-              </div>
-
-              <div className='fr-col-12 fr-mt-3w fr-mt-md-0 fr-col-md-6 fr-pl-md-3w'>
-                <DateInput
-                  isRequired
-                  label='Date de début'
-                  ariaLabel='date de commencement du statut'
-                  description='Date de début du statut'
-                  value={etape.statut === statutInput ? startDate : etape.date_debut}
-                  onValueChange={e => handleDateChange(e.target.value, etape.statut)}
-                />
-              </div>
+      {etapes.map((etape, index) => (
+        <div key={etape.statut} className='fr-grid-row fr-my-5w'>
+          <div className='fr-grid-row fr-col-11'>
+            <div className='fr-col-12 fr-col-md-6'>
+              <TextInput
+                isRequired
+                isDisabled
+                id={etape.statut}
+                label='Statut'
+                ariaLabel='statut du projet'
+                description='Statut du projet'
+                value={STATUS[etape.statut].label}
+              />
             </div>
-            <div className='fr-grid-row fr-grid-row--bottom fr-pl-2w fr-col-1 fr-pb-1w'>
-              <button
-                type='button'
-                aria-label='Supprimer l’étape'
-                className='delete-button fr-p-0'
-                disabled={etape.statut !== statutInput || etape.statut === 'investigation'}
-                onClick={() => onDelete(etape, index)}
-              >
-                <span className='fr-icon-delete-line' aria-hidden='true' />
-              </button>
+
+            <div className='fr-col-12 fr-mt-3w fr-mt-md-0 fr-col-md-6 fr-pl-md-3w'>
+              <DateInput
+                isRequired
+                name={etape.statut}
+                label='Date de début'
+                ariaLabel='date de commencement du statut'
+                description='Date de début du statut'
+                value={etape.statut === statutInput ? startDate : etape.date_debut || ''}
+                onValueChange={handleDateChange}
+              />
             </div>
           </div>
-        )
-      }
+          <div className='fr-grid-row fr-grid-row--bottom fr-pl-2w fr-col-1 fr-pb-1w'>
+            <button
+              type='button'
+              aria-label='Supprimer l’étape'
+              className='delete-button fr-p-0'
+              disabled={etape.statut !== statutInput || etape.statut === 'investigation'}
+              onClick={() => onDelete(etape, index)}
+            >
+              <span className='fr-icon-delete-line' aria-hidden='true' />
+            </button>
+          </div>
+        </div>
+      )
       )}
 
-      {Boolean(startDate || (startDate && statutInput !== 'obsolete')) && (
+      {(startDate && etapes.length < statusKeys.length) && (
         <Button
           label='Ajouter une étape'
           icon='add-circle-fill'
--- components/suivi-form/livrables/utils/select-options.js
+++ components/suivi-form/livrables/utils/select-options.js
@@ -18,41 +18,12 @@ export const licenceOptions = [
   {label: 'Fermé', value: 'ferme'}
 ]
 
-export const publicationOptions = [
-  {label: 'Accès via FTP', value: 'ftp'},
-  {label: 'Accès via un service cloud (oneDrive...)', value: 'cloud'},
-  {label: 'Accès via service HTTP(S)', value: 'http'},
-  {label: 'Aucun moyen d’accès en ligne', value: 'inexistante'}
-]
-
-export const systRefSpatialOptions = [
-  {label: 'EPSG:2154', value: 'EPSG:2154'},
-  {label: 'EPSG:3942', value: 'EPSG:3942'},
-  {label: 'EPSG:3943', value: 'EPSG:3943'},
-  {label: 'EPSG:3944', value: 'EPSG:3944'},
-  {label: 'EPSG:3945', value: 'EPSG:3945'},
-  {label: 'EPSG:3946', value: 'EPSG:3946'},
-  {label: 'EPSG:3947', value: 'EPSG:3947'},
-  {label: 'EPSG:3948', value: 'EPSG:3948'},
-  {label: 'EPSG:3949', value: 'EPSG:3949'},
-  {label: 'EPSG:3950', value: 'EPSG:3950'},
-  {label: 'EPSG:32620', value: 'EPSG:32620'},
-  {label: 'EPSG:5490', value: 'EPSG:5490'},
-  {label: 'EPSG:2971', value: 'EPSG:2971'},
-  {label: 'EPSG:2975', value: 'EPSG:2975'},
-  {label: 'EPSG:4471', value: 'EPSG:4471'}
-]
-
 export function getNatures(value) {
   return getLabelFromValue(value, natureOptions)
 }
 
 export function getDiffusions(value) {
   return getLabelFromValue(value, diffusionOptions)
-}
-
-export function getPublications(value) {
-  return getLabelFromValue(value, publicationOptions)
 }
 
 export function getLicences(value) {
