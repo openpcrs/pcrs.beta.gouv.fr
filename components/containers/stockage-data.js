c5980c982db8c95b1115edfb65cf8f56a14d74cd
Marine Le Pennec <lepennec.marine@gmail.com>
2023-10-18 16:01:37 +0200

    improve stockage data

--- components/containers/stockage-data.js
+++ components/containers/stockage-data.js
@@ -1,8 +1,9 @@
 import {useState} from 'react'
 import PropTypes from 'prop-types'
 
-import Badge from '@/components/badge.js'
 import colors from '@/styles/colors.js'
+
+import Badge from '@/components/badge.js'
 
 const StockageData = ({isPublic, type, params}) => {
   const [isParamsShow, setIsParamsShow] = useState(false)
@@ -11,32 +12,43 @@ const StockageData = ({isPublic, type, params}) => {
 
   return (
     <div>
-      <div className='fr-mt-1w fr-grid-row'><div className='title fr-mr-1w'>Type de stockage :</div><span>{type}</span></div>
-      <div className='fr-mt-1w fr-grid-row'>
-        <div className='title fr-mr-1w'>Statut du stockage:</div>
-        <Badge background={isPublic ? colors.success425 : colors.warningMain525} textColor='white'> {isPublic ? 'Public' : 'Privé'} </Badge>
+      <div className='fr-grid-row fr-grid-row--middle'>
+        <span className='fr-icon-database-line fr-mr-1w' aria-hidden='true' />
+        <h3 className='section-title fr-text--lead fr-m-0'>Stockage</h3>
       </div>
 
-      {isPublic && (
-        <div className='fr-grid-row' onClick={handleParamsShow}>
-          <div className='fr-grid-row fr-col-12'>
-            <div className='title fr-mr-1w fr-grid-row'>Afficher les paramètres du stockage</div><span className={`fr-icon-arrow-${isParamsShow ? 'down' : 'right'}-s-line`} />
-          </div>
-
-          {isParamsShow && (
-            <div className='fr-col-12 params-wrapper fr-mt-1w fr-p-1w'>
-              <div className='fr-grid-row fr-text--sm'><div className='title-dropdown fr-mr-1w'>Adresse du serveur :</div><span>{params.host || 'Non renseigné'}</span></div>
-              <div className='fr-grid-row fr-text--sm'><div className='title-dropdown fr-mr-1w'>Identifiant :</div><span>{params.username || 'Non renseigné'}</span></div>
-              <div className='fr-grid-row fr-text--sm'><div className='title-dropdown fr-mr-1w'>Mot de passe :</div><span>{params.password || 'Non renseigné'}</span></div>
-              <div className='fr-grid-row fr-text--sm'><div className='title-dropdown fr-mr-1w'>Chemin du répertoire :</div><span>{params.startPath || '/ (défaut)'}</span></div>
-              <div className='fr-grid-row fr-text--sm'><div className='title-dropdown fr-mr-1w'>Port :</div><span>{params.port || 'Non renseigné'}</span></div>
-            </div>
-          )}
+      <div className='fr-mt-3w'>
+        <div className='fr-mt-1w fr-grid-row'><div className='data-title fr-mr-1w'>Type de stockage :</div><span>{type}</span></div>
+        <div className='fr-mt-1w fr-grid-row'>
+          <div className='data-title fr-mr-1w'>Statut du stockage:</div>
+          <Badge background={isPublic ? colors.success425 : colors.warningMain525} textColor='white'> {isPublic ? 'Public' : 'Privé'} </Badge>
         </div>
-      )}
+
+        {isPublic && (
+          <div className='fr-grid-row' onClick={handleParamsShow}>
+            <div className='fr-grid-row fr-col-12'>
+              <div className='data-title fr-mr-1w fr-grid-row'>Afficher les paramètres du stockage</div><span className={`fr-icon-arrow-${isParamsShow ? 'down' : 'right'}-s-line`} />
+            </div>
+
+            {isParamsShow && (
+              <div className='fr-col-12 params-wrapper fr-mt-1w fr-p-1w'>
+                <div className='fr-grid-row fr-text--sm'><div className='dropdown-data-title fr-mr-1w'>Adresse du serveur :</div><span>{params.host || 'Non renseigné'}</span></div>
+                <div className='fr-grid-row fr-text--sm'><div className='dropdown-data-title fr-mr-1w'>Identifiant :</div><span>{params.username || 'Non renseigné'}</span></div>
+                <div className='fr-grid-row fr-text--sm'><div className='dropdown-data-title fr-mr-1w'>Mot de passe :</div><span>{params.password || 'Non renseigné'}</span></div>
+                <div className='fr-grid-row fr-text--sm'><div className='dropdown-data-title fr-mr-1w'>Chemin du répertoire :</div><span>{params.startPath || '/ (défaut)'}</span></div>
+                <div className='fr-grid-row fr-text--sm'><div className='dropdown-data-title fr-mr-1w'>Port :</div><span>{params.port || 'Non renseigné'}</span></div>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
 
       <style jsx>{`
-       .title {
+       .section-title {
+         border-bottom: 4px solid ${colors.info425};
+       }
+
+       .data-title {
          color: ${colors.info425};
          font-weight: bold;
        }
@@ -49,7 +61,7 @@ const StockageData = ({isPublic, type, params}) => {
          background: ${colors.grey975};
        }
 
-       .title-dropdown {
+       .dropdown-data-title {
          color: ${colors.grey50};
          font-weight: bold;
        }
