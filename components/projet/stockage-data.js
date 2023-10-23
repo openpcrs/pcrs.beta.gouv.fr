b7b25c769f44eb0508428ef502163580b16606c2
Théophile Merlière <theophile.merliere@gmail.com>
2023-10-23 22:58:39 +0200

    Add <DropdownList> component

--- components/projet/stockage-data.js
+++ components/projet/stockage-data.js
@@ -1,73 +1,50 @@
-import {useState} from 'react'
 import PropTypes from 'prop-types'
 
+import DropdownList from '../containers/dropdown-list.js'
 import colors from '@/styles/colors.js'
+
+import {STOCKAGE_PARAMS} from '@/lib/utils/projet.js'
 
 import Badge from '@/components/badge.js'
 
-const StockageData = ({isPublic, type, params}) => {
-  const [isParamsShow, setIsParamsShow] = useState(false)
-  const handleParamsShow = () => setIsParamsShow(!isParamsShow)
+const StockageData = ({isPublic, type, params}) => (
+  <div className='fr-mt-3w'>
+    <div className='fr-grid-row fr-grid-row--middle'>
+      <span className='fr-icon-server-fill fr-mr-1w' aria-hidden='true' />
+      <h3 className='section-title fr-text--md fr-m-0'>Stockage</h3>
+    </div>
 
-  return (
-    <div className='fr-mt-3w'>
-      <div className='fr-grid-row fr-grid-row--middle'>
-        <span className='fr-icon-server-fill fr-mr-1w' aria-hidden='true' />
-        <h3 className='section-title fr-text--md fr-m-0'>Stockage</h3>
+    <div className='fr-mt-1w'>
+      <div className='fr-mt-1w fr-grid-row'><div className='data-title fr-mr-1w'>Type de stockage :</div><span>{type.toUpperCase()}</span></div>
+      <div className='fr-mt-1w fr-grid-row'>
+        <div className='data-title fr-mr-1w'>Statut du stockage:</div>
+        <Badge background={isPublic ? colors.success425 : colors.warningMain525} textColor='white'> {isPublic ? 'Public' : 'Privé'} </Badge>
       </div>
 
-      <div className='fr-mt-1w'>
-        <div className='fr-mt-1w fr-grid-row'><div className='data-title fr-mr-1w'>Type de stockage :</div><span>{type.toUpperCase()}</span></div>
-        <div className='fr-mt-1w fr-grid-row'>
-          <div className='data-title fr-mr-1w'>Statut du stockage:</div>
-          <Badge background={isPublic ? colors.success425 : colors.warningMain525} textColor='white'> {isPublic ? 'Public' : 'Privé'} </Badge>
-        </div>
+      {(isPublic && ['ftp', 'sftp'].includes(type)) && (
+        <DropdownList
+          title='Afficher les paramètres du stockage'
+          list={Object.keys(params).map(key => {
+            const {label, defaultValue} = STOCKAGE_PARAMS[key]
 
-        {(isPublic && ['ftp', 'sftp'].includes(type)) && (
-          <div className='fr-grid-row' onClick={handleParamsShow}>
-            <div className='fr-grid-row fr-col-12'>
-              <div className='data-title dropdown fr-mr-1w fr-grid-row'>Afficher les paramètres du stockage</div><span className={`fr-icon-arrow-${isParamsShow ? 'down' : 'right'}-s-line`} />
-            </div>
+            return {
+              label,
+              value: params[key] || defaultValue
+            }
+          })}
+          isDefaultOpen={false}
+        />
+      )}
+    </div>
 
-            {isParamsShow && (
-              <div className='fr-col-12 params-wrapper fr-mt-1w fr-p-1w'>
-                <div className='fr-grid-row fr-text--sm'><div className='dropdown-data-title fr-mr-1w'>Adresse du serveur :</div><span>{params.host || 'Non renseigné'}</span></div>
-                <div className='fr-grid-row fr-text--sm'><div className='dropdown-data-title fr-mr-1w'>Identifiant :</div><span>{params.username || 'Non renseigné'}</span></div>
-                <div className='fr-grid-row fr-text--sm'><div className='dropdown-data-title fr-mr-1w'>Mot de passe :</div><span>{params.password || 'Non renseigné'}</span></div>
-                <div className='fr-grid-row fr-text--sm'><div className='dropdown-data-title fr-mr-1w'>Chemin du répertoire :</div><span>{params.startPath || '/ (défaut)'}</span></div>
-                <div className='fr-grid-row fr-text--sm'><div className='dropdown-data-title fr-mr-1w'>Port :</div><span>{params.port || 'Non renseigné'}</span></div>
-              </div>
-            )}
-          </div>
-        )}
-      </div>
-
-      <style jsx>{`
+    <style jsx>{`
        .data-title {
          color: ${colors.info425};
          font-weight: bold;
        }
-
-       .dropdown {
-        cursor: pointer;
-       }
-
-       .content-wrapper span {
-         font-weight: normal;
-       }
-
-       .params-wrapper {
-         background: ${colors.grey975};
-       }
-
-       .dropdown-data-title {
-         color: ${colors.grey50};
-         font-weight: bold;
-       }
      `}</style>
-    </div>
-  )
-}
+  </div>
+)
 
 StockageData.propTypes = {
   type: PropTypes.string.isRequired,
