9011f687ab13de9c22b2e05ba13c1001432aba41
Théophile Merlière <theophile.merliere@gmail.com>
2023-10-24 14:37:01 +0200

    Store livrable in state instead of index

--- components/projet/livrables-section.js
+++ components/projet/livrables-section.js
@@ -9,42 +9,46 @@ import {livrableRenderItem} from '@/components/projet/list-render-items.js'
 import SelectInput from '@/components/select-input.js'
 
 const LivrablesSection = ({projectId, livrables}) => {
-  const [selectedLivrableIdx, setSelectedLivrableIdx] = useState(0)
+  const [selectedLivrable, setSelectedLivrable] = useState(livrables.length > 0 ? livrables[0] : null)
 
   const orderedLivrablesByPublication = sortBy(livrables, livrable =>
     livrable.date_livraison ? new Date(livrable.date_livraison) : 0
   )
 
-  const livrablesOptions = orderedLivrablesByPublication.map((item, idx) => ({
+  const livrablesOptions = orderedLivrablesByPublication.map(item => ({
     label: `${item.nom} - ${LIVRABLE_NATURES[item.nature].label}`,
-    value: idx
+    value: item.nom
   }))
 
-  const livrable = orderedLivrablesByPublication[selectedLivrableIdx]
+  const handleSelectedLivrable = event => {
+    const {value} = event.target
+    const selectedLivrable = livrables.find(({nom}) => nom === value)
+    setSelectedLivrable(selectedLivrable)
+  }
 
   return (
     <div>
       <h3 className='fr-text--lead fr-mt-5w fr-mb-3w'>Livrables : {livrables.length}</h3>
       <div>
         <SelectInput
-          value={selectedLivrableIdx}
-          label={livrable.nom}
+          value={selectedLivrable.nom}
+          label={selectedLivrable.nom}
           options={livrablesOptions}
           isDisabled={livrables.length === 1}
-          onValueChange={e => setSelectedLivrableIdx(e.target.value)}
+          onValueChange={handleSelectedLivrable}
         />
 
         <div>
-          {livrableRenderItem(orderedLivrablesByPublication[selectedLivrableIdx])}
+          {livrableRenderItem(selectedLivrable)}
 
-          {livrable.stockage_id && (
+          {selectedLivrable.stockage_id && (
             <div className='stockage-preview'>
               <StockagePreview
                 projectId={projectId}
-                stockageId={livrable.stockage_id}
-                isStockagePublic={livrable.stockage_public}
-                isDownloadable={livrable.stockage_telechargement}
-                params={livrable.stockage_params}
+                stockageId={selectedLivrable.stockage_id}
+                isStockagePublic={selectedLivrable.stockage_public}
+                isDownloadable={selectedLivrable.stockage_telechargement}
+                params={selectedLivrable.stockage_params}
               />
             </div>
           )}
