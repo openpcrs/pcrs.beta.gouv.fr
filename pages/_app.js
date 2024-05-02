3eeacd0f8383fb345e4a6a30992aaf575159d747
Guillaume <guillaume@livingdata.co>
2024-05-02 17:37:42 +0200

    Add matomo function

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import PropTypes from 'prop-types'
+
+import {init} from '@socialgouv/matomo-next'
 
 import {DeviceContextProvider} from '@/contexts/device.js'
 import {AuthentificationContextProvider} from '@/contexts/authentification-token.js'
@@ -20,15 +22,28 @@ import '@gouvfr/dsfr/dist/utility/icons/icons-buildings/icons-buildings.min.css'
 import '@gouvfr/dsfr/dist/utility/icons/icons-design/icons-design.min.css'
 import '@gouvfr/dsfr/dist/utility/icons/icons-logo/icons-logo.min.css'
 
-const App = ({Component, pageProps}) => (
-  <React.StrictMode>
-    <DeviceContextProvider>
-      <AuthentificationContextProvider>
-        <Component {...pageProps} />
-      </AuthentificationContextProvider>
-    </DeviceContextProvider>
-  </React.StrictMode>
-)
+const MATOMO_URL = process.env.NEXT_PUBLIC_MATOMO_URL
+const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID
+
+const App = ({Component, pageProps}) => {
+  useEffect(() => {
+    init({
+      url: MATOMO_URL,
+      siteId: MATOMO_SITE_ID,
+      diableCookies: true
+    })
+  }, [])
+
+  return (
+    <React.StrictMode>
+      <DeviceContextProvider>
+        <AuthentificationContextProvider>
+          <Component {...pageProps} />
+        </AuthentificationContextProvider>
+      </DeviceContextProvider>
+    </React.StrictMode>
+  )
+}
 
 App.propTypes = {
   Component: PropTypes.any.isRequired,
