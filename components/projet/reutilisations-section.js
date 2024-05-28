7c4ddd92af9f8171136ca766c617445b95a5ecc7
Guillaume <guillaume@livingdata.co>
2024-05-28 14:45:43 +0200

    Change reutilisations style

--- components/projet/reutilisations-section.js
+++ components/projet/reutilisations-section.js
@@ -3,24 +3,31 @@ import PropTypes from 'prop-types'
 const Reutilisations = ({reutilisations}) => (
   <>
     <h3 className='fr-text--lead fr-mt-5w fr-mb-3w'>Réutilisations : {reutilisations?.length}</h3>
-    <div>
+    <div className='grid-container'>
       {reutilisations ? (
         reutilisations.map(reutilisation => (
-          <div key={reutilisation.lien} className='fr-grid-row fr-mb-3w'>
-            <div className='fr-col-12'>
+          <div key={reutilisation.lien} className='fr-mb-3w flex-container'>
+            <div className='flex-child'>
               <div className='fr-card fr-enlarge-link'>
                 <div className='fr-card__body'>
                   <div className='fr-card__content'>
                     <h3 className='fr-card__title'>
-                      <a target='_blank' rel='noreferrer' href={reutilisation.lien}>
+                      <a
+                        href={reutilisation.lien}
+                        target='_blank'
+                        rel='noopener noreferrer'
+                      >
                         {reutilisation.titre}
                       </a>
                     </h3>
                     <div className='fr-card__desc'>
-                      {reutilisation.description.split('\n').map((line, idx) => (
-                        // eslint-disable-next-line react/no-array-index-key
-                        <div key={idx}>{line}</div>
-                      ))}</div>
+                      {reutilisation.description && (
+                        reutilisation.description.split('\n').map((line, idx) => (
+                          // eslint-disable-next-line react/no-array-index-key
+                          <div key={idx}>{line}</div>
+                        ))
+                      )}
+                    </div>
                   </div>
                 </div>
               </div>
@@ -31,6 +38,23 @@ const Reutilisations = ({reutilisations}) => (
         <div>Aucune réutilisation renseignée</div>
       )}
     </div>
+    <style jsx>{`
+      .grid-container {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+        gap: 1em;
+      }
+
+      .flex-container {
+        display: flex;
+        flex-direction: column;
+        max-width: 500px;
+      }
+
+      .flex-child {
+        flex: 1;
+      }
+    `}</style>
   </>
 )
 
