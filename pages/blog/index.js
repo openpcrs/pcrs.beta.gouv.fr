757db45c0295b1c79064f5446bb11395287da844
FLacombe <francois.lacombe@infos-reseaux.com>
2025-01-15 10:31:22 +0100

    Make blog static (#487)
    Passage du blog en statique, suppression de la pagination, des références à ghost et reprise des images
    
    ---------
    
    Co-authored-by: Guillaume <guillaume@livingdata.co>

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,119 +1,116 @@
-import PropTypes from 'prop-types'
+import {useRouter} from 'next/router'
 import Image from 'next/image'
 
-import {getPosts, getTags} from '@/lib/blog.js'
+import {getStaticPosts, getTags} from '@/lib/blog-static.js'
 
 import colors from '@/styles/colors.js'
 
 import Page from '@/layouts/main.js'
 
-import Pagination from '@/components/pagination.js'
+// Pas de pagination dans le fichier JSON // import Pagination from '@/components/pagination.js'
 import BlogCard from '@/components/post-card.js'
 import BlogTags from '@/components/blog/blog-tags.js'
 
-const Blog = ({posts, tags, tagsList, pagination}) => (
-  <Page title='Blog du PCRS' description='La liste complète des billets du blog autour du PCRS'>
-    {posts ? (
-      <div>
-        <div className='blog-header fr-my-5w'>
-          <Image
-            src='/images/illustrations/blog_illustration.svg'
-            height={200}
-            width={200}
-            alt=''
-            aria-hidden='true'
-          />
-          <h2 className='fr-mt-5w fr-mb-0'>Blog du PCRS</h2>
-        </div>
+const Blog = () => {
+  const router = useRouter()
+  const {query} = router
+  const tagsList = getTags()
+  const tags = query.tags?.split(',') || []
+  const posts = getStaticPosts().filter(post => tags.length === 0 || post.tags.some(tag => tags.includes(tag.name.toLowerCase().replace('é', 'e'))))
 
-        <div className='fr-grid-row fr-grid-row--center'>
-          {tagsList.length > 0 && <BlogTags selectedTags={tags} tagsList={tagsList} />}
-          <div className='fr-grid-row fr-mb-6w fr-px-1w fr-px-md-5w'>
-            {posts.length > 0 && posts.map(post => (
-              <div key={post.id} className='fr-col-12 fr-col-md-6 fr-col-lg-3 fr-col-lg-4 fr-p-md-3w'>
-                <BlogCard post={post} />
-              </div>
-            ))}
-            {(posts.length === 0 && tags.length === 0) && <div className='no-article'>Aucun article de blog n’est disponible</div>}
-            {(posts.length === 0 && tags.length > 0) && <div className='no-article'>Aucun article ne contient ces tags</div>}
+  return (
+    <Page title='Blog du PCRS' description='La liste complète des billets du blog autour du PCRS'>
+      {posts ? (
+        <div>
+          <div className='blog-header fr-my-5w'>
+            <Image
+              src='/images/illustrations/blog_illustration.svg'
+              height={200}
+              width={200}
+              alt=''
+              aria-hidden='true'
+            />
+            <h2 className='fr-mt-5w fr-mb-0'>Blog du PCRS</h2>
           </div>
 
-          <Pagination {...pagination} />
+          <div className='fr-grid-row fr-grid-row--center'>
+            {tagsList.length > 0 && <BlogTags selectedTags={tags} tagsList={tagsList} />}
+            <div className='fr-grid-row fr-mb-6w fr-px-1w fr-px-md-5w'>
+              {posts.length > 0 && posts.map(post => (
+                <div key={post.id} className='fr-col-12 fr-col-md-6 fr-col-lg-3 fr-col-lg-4 fr-p-md-3w'>
+                  <BlogCard post={post} />
+                </div>
+              ))}
+              {(posts.length === 0 && tags.length === 0) && <div className='no-article'>Aucun article de blog n’est disponible</div>}
+              {(posts.length === 0 && tags.length > 0) && <div className='no-article'>Aucun article ne contient ces tags</div>}
+            </div>
+
+            {/* Pas de pagination dans le fichier JSON */}
+            {/* <Pagination {...pagination} /> */}
+          </div>
         </div>
-      </div>
-    ) : (
-      <div className='unavailable fr-p-5w'>
-        <Image
-          src='/images/illustrations/500.png'
-          height={456}
-          width={986}
-          alt=''
-          style={{
-            width: '100%',
-            maxWidth: '500px',
-            height: 'auto'
-          }}
-        />
+      ) : (
+        <div className='unavailable fr-p-5w'>
+          <Image
+            src='/images/illustrations/500.png'
+            height={456}
+            width={986}
+            alt=''
+            style={{
+              width: '100%',
+              maxWidth: '500px',
+              height: 'auto'
+            }}
+          />
 
-        <div className='fr-pt-8w'>
-          <p>
-            <b className='fr-h3'>Le blog est actuellement inaccessible</b><br />
-            <i>Merci de réessayer ultérieurement</i>
-          </p>
+          <div className='fr-pt-8w'>
+            <p>
+              <b className='fr-h3'>Le blog est actuellement inaccessible</b><br />
+              <i>Merci de réessayer ultérieurement</i>
+            </p>
+          </div>
         </div>
-      </div>
-    )}
+      )}
 
-    <style jsx>{`
-      .blog-header {
-        text-align: center;
-      }
+      <style jsx>{`
+        .blog-header {
+          text-align: center;
+        }
 
-      .blog-cards-list {
-        display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-        gap: 6em;
-        justify-items: center;
-      }
+        .blog-cards-list {
+          display: grid;
+          grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+          gap: 6em;
+          justify-items: center;
+        }
 
-      .no-article {
-        font-style: italic;
-      }
+        .no-article {
+          font-style: italic;
+        }
 
-      .unavailable {
-        text-align: center;
-        color: ${colors.darkgrey};
-      }
+        .unavailable {
+          text-align: center;
+          color: ${colors.darkgrey};
+        }
     `}</style>
-  </Page>
-)
-
-export async function getServerSideProps({query}) {
-  const data = await getPosts(query)
-  const tags = query?.tags || null
-  const tagsList = await getTags()
-
-  return {
-    props: {
-      posts: data?.posts || null,
-      pagination: data?.meta.pagination || null,
-      tags: tags?.split(',') || [],
-      tagsList
-    }
-  }
+    </Page>
+  )
 }
 
-Blog.propTypes = {
-  posts: PropTypes.array,
-  pagination: PropTypes.object,
-  tags: PropTypes.array,
-  tagsList: PropTypes.array.isRequired
-}
-
-Blog.defaultProps = {
-  posts: null,
-  pagination: null,
-  tags: null
-}
+// Au revoir blog dynamique !
+// export async function getServerSideProps({query}) {
+//   const data = await getPosts(query)
+//   const tags = query?.tags || null
+//   const tagsList = await getTags()
+//
+//   return {
+//     props: {
+//       posts: data?.posts || null,
+//       pagination: data?.meta.pagination || null,
+//       tags: tags?.split(',') || [],
+//       tagsList
+//     }
+//   }
+// }
 
 export default Blog
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
-import PropTypes from 'prop-types'
 import Image from 'next/image'
 
 import Page from '@/layouts/main.js'
 
 import colors from '@/styles/colors.js'
-
-import {getPosts} from '@/lib/blog.js'
 
 import Hero from '@/components/hero.js'
 import SectionImage from '@/components/section-image.js'
@@ -13,67 +10,73 @@ import Section from '@/components/section.js'
 import Button from '@/components/button.js'
 import EventCarousel from '@/components/event-carousel.js'
 import PostCard from '@/components/post-card.js'
+import {getStaticPosts} from '@/lib/blog-static.js'
 
-const Home = ({testimonies}) => (
-  <Page>
-    <EventCarousel />
-    <Hero />
-    <>
-      <p className='intro fr-p-3w fr-p-md-7w fr-text--lg'>
-        Vous êtes sur le site construit par et pour les territoires. Il expose les différents projets PCRS en cours, leurs acteurs et résultats produits. C’est le support de l’animation nationale associé à la documentation à produire.
-      </p>
+const Home = () => {
+  const testimonies = getStaticPosts().filter(p =>
+    p.tags.some(tag => tag.name === 'Témoignage')
+  ) || []
 
-      <style jsx>{`
-          .intro {
-            background: ${colors.darkgrey};
-            color: white;
-            text-align: center;
-          }
-        `}</style>
-    </>
+  return (
+    <Page>
+      <EventCarousel />
+      <Hero />
+      <>
+        <p className='intro fr-p-3w fr-p-md-7w fr-text--lg'>
+          Vous êtes sur le site construit par et pour les territoires. Il expose les différents projets PCRS en cours, leurs acteurs et résultats produits. C’est le support de l’animation nationale associé à la documentation à produire.
+        </p>
 
-    <SectionImage
-      title='Documentation'
-      imageLink='/images/illustrations/doc_illustration.svg'
-      id='documentation'
-    >
-      <p>
-        Accompagné par l’ANCT, vous avez tous les éléments en main pour mettre au point le PCRS sur votre territoire. Vous pouvez accéder au contexte réglementaire sur <a className='fr-link fr-icon-external-link-line fr-link--icon-right' href=' https://docs.pcrs.beta.gouv.fr/contexte-reglementaire/reglementation'>cette page</a>.
-      </p>
-      <p>
-        Un ensemble de fiches pratiques est disponible en fonction des problématiques que vous pouvez rencontrer, associées à une foire aux questions pour répondre aux interrogations les plus fréquentes.
-        Si vous ne trouvez pas le contenu correspondant à vos attentes, n’hésitez pas à nous contacter.
-      </p>
+        <style jsx>{`
+.intro {
+background: ${colors.darkgrey};
+color: white;
+text-align: center;
+}
+`}</style>
+      </>
 
-      <div className='doc-links'>
-        <ul className='doc-list'>
-          <li>
-            <a className='fr-link fr-icon-article-fill fr-link--icon-left' href='https://docs.pcrs.beta.gouv.fr/contexte-reglementaire/geostandards'>
-              Standards
-            </a>
-          </li>
-          <li>
-            <a className='fr-link fr-icon-article-fill fr-link--icon-left' href='https://docs.pcrs.beta.gouv.fr/contexte-reglementaire/reglementation'>
-              Règlementation
-            </a>
-          </li>
-          <li>
-            <a className='fr-link fr-icon-article-fill fr-link--icon-left' href='https://docs.pcrs.beta.gouv.fr/organiser-son-projet-pcrs/construction'>
-              Bonnes pratiques
-            </a>
-          </li>
-        </ul>
+      <SectionImage
+        title='Documentation'
+        imageLink='/images/illustrations/doc_illustration.svg'
+        id='documentation'
+      >
+        <p>
+          Accompagné par l’ANCT, vous avez tous les éléments en main pour mettre au point le PCRS sur votre territoire. Vous pouvez accéder au contexte réglementaire sur <a className='fr-link fr-icon-external-link-line fr-link--icon-right' href=' https://docs.pcrs.beta.gouv.fr/contexte-reglementaire/reglementation'>cette page</a>.
+        </p>
+        <p>
+          Un ensemble de fiches pratiques est disponible en fonction des problématiques que vous pouvez rencontrer, associées à une foire aux questions pour répondre aux interrogations les plus fréquentes.
+          Si vous ne trouvez pas le contenu correspondant à vos attentes, n’hésitez pas à nous contacter.
+        </p>
 
-        <Button
-          isExternal
-          href='https://docs.pcrs.beta.gouv.fr/'
-          label='Accéder à la documentation'
-        >
-          Voir la documentation
-        </Button>
-      </div>
+        <div className='doc-links'>
+          <ul className='doc-list'>
+            <li>
+              <a className='fr-link fr-icon-article-fill fr-link--icon-left' href='https://docs.pcrs.beta.gouv.fr/contexte-reglementaire/geostandards'>
+                Standards
+              </a>
+            </li>
+            <li>
+              <a className='fr-link fr-icon-article-fill fr-link--icon-left' href='https://docs.pcrs.beta.gouv.fr/contexte-reglementaire/reglementation'>
+                Règlementation
+              </a>
+            </li>
+            <li>
+              <a className='fr-link fr-icon-article-fill fr-link--icon-left' href='https://docs.pcrs.beta.gouv.fr/organiser-son-projet-pcrs/construction'>
+                Bonnes pratiques
+              </a>
+            </li>
+          </ul>
 
-      <style jsx>{`
+          <Button
+            isExternal
+            href='https://docs.pcrs.beta.gouv.fr/'
+            label='Accéder à la documentation'
+          >
+            Voir la documentation
+          </Button>
+        </div>
+
+        <style jsx>{`
           p, .doc-links{
             text-align: left;
           }
@@ -92,195 +95,177 @@ const Home = ({testimonies}) => (
             gap: 10px;
           }
         `}</style>
-    </SectionImage>
+      </SectionImage>
 
-    <SectionImage
-      title='Feuille de route'
-      subtitle='Les étapes suivantes marqueront le développement de ce portail'
-      imageLink='/images/illustrations/progress_illustration.svg'
-      id='feuille-de-route'
-      background='secondary'
-      imageSide='right'
-    >
-      <ul className='fr-text--sm dev-list'>
-        <li>
-          <span className='fr-icon-git-branch-fill' /> Mise en ligne d’une cartographie des projets et d’un annuaire des APLC complétés de manière collaborative.
-        </li>
-        <li>
-          <span className='fr-icon-git-branch-fill' /> Consolidation de la documentation en continu
-        </li>
-        <li>
-          <span className='fr-icon-git-branch-fill' /> Construction d’un cadre de financement commun
-        </li>
-        <li>
-          <span className='fr-icon-git-branch-fill' /> Mise en valeur des PCRS d’ores et déjà disponibles sous licence ouverte.
-        </li>
-      </ul>
-
-      <style jsx>{`
-          .dev-list {
-            text-align: left;
-            display: grid;
-            justify-content: center;
-            gap: 10px;
-          }
-        `}</style>
-    </SectionImage>
-
-    <SectionImage
-      title='Événements autour du PCRS'
-      background='blue'
-      imageLink='/images/illustrations/calendar_illustration.svg'
-      id='evenements'
-    >
-      <div className='fr-p-2w fr-p-lg-4w fr-grid-row fr-grid-row--center'>
-        <p className='fr-py-1w'>
-          Tenez-vous informé des prochains événements organisés par l’ANCT à propos du PCRS. Ateliers, conférences, annonces pour être toujours à jour.
-        </p>
-        <Button
-          isWhite
-          href='/evenements'
-          buttonStyle='secondary'
-          label='Évenement à venir'
-        >
-          Événements à venir
-        </Button>
-      </div>
-    </SectionImage>
-
-    {testimonies.length > 0 && (
-      <Section
-        title='Témoignages autour du PCRS'
+      <SectionImage
+        title='Feuille de route'
+        subtitle='Les étapes suivantes marqueront le développement de ce portail'
+        imageLink='/images/illustrations/progress_illustration.svg'
+        id='feuille-de-route'
         background='secondary'
-        id='testimonies'
-        style={{
-          textAlign: 'center'
-        }}
+        imageSide='right'
       >
-        <Image
-          src='/images/illustrations/testimony_illustration.svg'
-          height={250}
-          width={250}
-          alt=''
-          className='fr-my-5w'
-        />
-        <div className='fr-p-2w fr-p-lg-4w fr-container--fluid'>
-          <div className='fr-grid-row fr-grid-row--gutters fr-my-5w'>
-            {testimonies.map(testimony => (
-              <div key={testimony.id} className='card-container fr-col-12 fr-col-lg'>
-                <PostCard isTestimony post={testimony} />
-              </div>
-            )
-            )}
-          </div>
-
-          <Button href='/temoignages' label='Lire tous les témoignages'>
-            Lire tous les témoignages
-          </Button>
-        </div>
+        <ul className='fr-text--sm dev-list'>
+          <li>
+            <span className='fr-icon-git-branch-fill' /> Mise en ligne d’une cartographie des projets et d’un annuaire des APLC complétés de manière collaborative.
+          </li>
+          <li>
+            <span className='fr-icon-git-branch-fill' /> Consolidation de la documentation en continu
+          </li>
+          <li>
+            <span className='fr-icon-git-branch-fill' /> Construction d’un cadre de financement commun
+          </li>
+          <li>
+            <span className='fr-icon-git-branch-fill' /> Mise en valeur des PCRS d’ores et déjà disponibles sous licence ouverte.
+          </li>
+        </ul>
 
         <style jsx>{`
-          .card-container {
-            text-align: start;
-          }
+        .dev-list {
+          text-align: left;
+          display: grid;
+          justify-content: center;
+          gap: 10px;
+        }
       `}</style>
-      </Section>
-    )}
+      </SectionImage>
 
-    <div>
       <SectionImage
-        title='Contactez-nous'
-        subtitle='Vous ne trouvez pas les réponses à vos questions sur ce site ou dans la documentation ?'
-        id='contact'
-        imageLink='/images/illustrations/contact_illustration.svg'
+        title='Événements autour du PCRS'
+        background='blue'
+        imageLink='/images/illustrations/calendar_illustration.svg'
+        id='evenements'
       >
-        <div className='fr-grid-row'>
-          <div className='fr-col-12'>
-            <h3 className='fr-text--lg fr-mb-1w'>Envoyez nous un email</h3>
-            <p className='fr-text--sm'>Notre équipe fera le nécessaire pour vous répondre dans les plus brefs délais, dans la limite de sa disponibilité.</p>
-            <div className='fr-grid-row fr-grid-row--center'>
-              <Button
-                isExternal
-                href='mailto:contact@pcrs.beta.gouv.fr'
-                label='Contacter l’équipe'
-                size='sm'
-              >
-                <span className='fr-icon-mail-line' aria-hidden='true' />&nbsp;Contactez-nous
-              </Button>
-            </div>
-          </div>
+        <div className='fr-p-2w fr-p-lg-4w fr-grid-row fr-grid-row--center'>
+          <p className='fr-py-1w'>
+            Tenez-vous informé des prochains événements organisés par l’ANCT à propos du PCRS. Ateliers, conférences, annonces pour être toujours à jour.
+          </p>
+          <Button
+            isWhite
+            href='/evenements'
+            buttonStyle='secondary'
+            label='Évenement à venir'
+          >
+            Événements à venir
+          </Button>
+        </div>
+      </SectionImage>
 
-          <div className='fr-col-12 fr-py-7w'>
-            <h3 className='fr-text--lg fr-mb-1w'>Rejoignez notre forum</h3>
-            <p className='fr-text--sm'>Un lieu d’échange entre <b>porteurs de projets</b>, <b>animateurs</b> et <b>utilisateurs des livrables</b> du PCRS. Venez nous rencontrer pour présenter <b>vos problématiques</b> et vous inspirer d’autres solutions dans les échanges existants</p>
-            <div className='fr-grid-row fr-grid-row--center'>
-              <Button
-                isExternal
-                href='https://forum.pcrs.beta.gouv.fr/'
-                label='Rejoindre le forum'
-                size='sm'
-              >
-                <span className='fr-icon-question-answer-line' aria-hidden='true' />&nbsp;Rejoindre le forum
-              </Button>
+      {testimonies.length > 0 && (
+        <Section
+          title='Témoignages autour du PCRS'
+          background='secondary'
+          id='testimonies'
+          style={{
+            textAlign: 'center'
+          }}
+        >
+          <Image
+            src='/images/illustrations/testimony_illustration.svg'
+            height={250}
+            width={250}
+            alt=''
+            className='fr-my-5w'
+          />
+          <div className='fr-p-2w fr-p-lg-4w fr-container--fluid'>
+            <div className='fr-grid-row fr-grid-row--gutters fr-my-5w'>
+              {testimonies.map(testimony => (
+                <div key={testimony.id} className='card-container fr-col-12 fr-col-lg'>
+                  <PostCard isTestimony post={testimony} />
+                </div>
+              )
+              )}
             </div>
-          </div>
 
-          <div className='fr-col-12 fr-p-0'>
-            <h3 className='fr-text--lg'>Suivez-nous sur les réseaux sociaux</h3>
-
-            <div className='fr-grid-row fr-grid-row--center fr-grid-row--middle fr-grid-row--gutters fr-mt-3w'>
-              <div className='fr-grid-row fr-grid-row--center fr-col-12 fr-col-lg-5'>
-                <Button
-                  isExternal
-                  href='https://twitter.com/pcrsbeta'
-                  label='Nous suivre sur Twitter'
-                  size='sm'
-                  iconSide='left'
-                >
-                  <span className='fr-icon-twitter-fill' aria-hidden='true' />&nbsp;Nous suivre sur Twitter
-                </Button>
-              </div>
-              <div className='fr-grid-row fr-grid-row--center fr-col-12 fr-col-lg-5'>
-                <Button
-                  isExternal
-                  href='https://www.linkedin.com/company/pcrs-beta-gouv-fr/about/'
-                  label='Nous suivre sur Linkedin'
-                  size='sm'
-                >
-                  <span className='fr-icon-linkedin-box-fill' aria-hidden='true' />&nbsp;Nous suivre sur Linkedin
-                </Button>
-              </div>
-            </div>
+            <Button href='/temoignages' label='Lire tous les témoignages'>
+              Lire tous les témoignages
+            </Button>
           </div>
 
           <style jsx>{`
-          .social-media {
-            background: transparent;
-          }
-        `}</style>
-        </div>
-      </SectionImage>
-    </div>
-  </Page>
-)
-
-export async function getServerSideProps({query}) {
-  const data = await getPosts({...query, tags: 'temoignage'})
-  const lastThreeTestimonies = data?.posts.slice(0, 3)
-
-  return {
-    props: {
-      testimonies: lastThreeTestimonies || []
-    }
-  }
+.card-container {
+text-align: start;
 }
+`}</style>
+        </Section>
+      )}
 
-Home.propTypes = {
-  testimonies: PropTypes.array
-}
+      <div>
+        <SectionImage
+          title='Contactez-nous'
+          subtitle='Vous ne trouvez pas les réponses à vos questions sur ce site ou dans la documentation ?'
+          id='contact'
+          imageLink='/images/illustrations/contact_illustration.svg'
+        >
+          <div className='fr-grid-row'>
+            <div className='fr-col-12'>
+              <h3 className='fr-text--lg fr-mb-1w'>Envoyez nous un email</h3>
+              <p className='fr-text--sm'>Notre équipe fera le nécessaire pour vous répondre dans les plus brefs délais, dans la limite de sa disponibilité.</p>
+              <div className='fr-grid-row fr-grid-row--center'>
+                <Button
+                  isExternal
+                  href='mailto:contact@pcrs.beta.gouv.fr'
+                  label='Contacter l’équipe'
+                  size='sm'
+                >
+                  <span className='fr-icon-mail-line' aria-hidden='true' />&nbsp;Contactez-nous
+                </Button>
+              </div>
+            </div>
 
-Home.defaultProps = {
-  testimonies: []
+            <div className='fr-col-12 fr-py-7w'>
+              <h3 className='fr-text--lg fr-mb-1w'>Rejoignez notre forum</h3>
+              <p className='fr-text--sm'>Un lieu d’échange entre <b>porteurs de projets</b>, <b>animateurs</b> et <b>utilisateurs des livrables</b> du PCRS. Venez nous rencontrer pour présenter <b>vos problématiques</b> et vous inspirer d’autres solutions dans les échanges existants</p>
+              <div className='fr-grid-row fr-grid-row--center'>
+                <Button
+                  isExternal
+                  href='https://forum.pcrs.beta.gouv.fr/'
+                  label='Rejoindre le forum'
+                  size='sm'
+                >
+                  <span className='fr-icon-question-answer-line' aria-hidden='true' />&nbsp;Rejoindre le forum
+                </Button>
+              </div>
+            </div>
+
+            <div className='fr-col-12 fr-p-0'>
+              <h3 className='fr-text--lg'>Suivez-nous sur les réseaux sociaux</h3>
+
+              <div className='fr-grid-row fr-grid-row--center fr-grid-row--middle fr-grid-row--gutters fr-mt-3w'>
+                <div className='fr-grid-row fr-grid-row--center fr-col-12 fr-col-lg-5'>
+                  <Button
+                    isExternal
+                    href='https://twitter.com/pcrsbeta'
+                    label='Nous suivre sur Twitter'
+                    size='sm'
+                    iconSide='left'
+                  >
+                    <span className='fr-icon-twitter-fill' aria-hidden='true' />&nbsp;Nous suivre sur Twitter
+                  </Button>
+                </div>
+                <div className='fr-grid-row fr-grid-row--center fr-col-12 fr-col-lg-5'>
+                  <Button
+                    isExternal
+                    href='https://www.linkedin.com/company/pcrs-beta-gouv-fr/about/'
+                    label='Nous suivre sur Linkedin'
+                    size='sm'
+                  >
+                    <span className='fr-icon-linkedin-box-fill' aria-hidden='true' />&nbsp;Nous suivre sur Linkedin
+                  </Button>
+                </div>
+              </div>
+            </div>
+
+            <style jsx>{`
+              .social-media {
+                background: transparent;
+              }
+          `}</style>
+          </div>
+        </SectionImage>
+      </div>
+    </Page>
+  )
 }
 
 export default Home
