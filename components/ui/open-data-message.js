315e90118f59ad28d3ed7dfa229160aa318f2270
FLacombe <francois.lacombe@infos-reseaux.com>
2025-01-29 10:21:13 +0100

    Ajout message mise à jour

--- components/ui/open-data-message.js
+++ components/ui/open-data-message.js
@@ -5,7 +5,7 @@ const OpenDataMessage = () => (
     className='fr-p-1w fr-text--sm fr-m-0 fr-grid-row fr-grid-row--middle fr-grid-row--center'
   >
     <div>
-      Les données de cette carte sont disponibles publiquement sur le site&nbsp;<a rel='noreferrer' href='https://www.data.gouv.fr/fr/organizations/pcrs-beta-gouv-fr/' target='_blank' title='ouvre un onglet vers data.gouv.fr'>data.gouv.fr</a>
+      Les données de cette carte sont disponibles sur le site&nbsp;<a rel='noreferrer' href='https://www.data.gouv.fr/fr/organizations/pcrs-beta-gouv-fr/' target='_blank' title='ouvre un onglet vers data.gouv.fr'>data.gouv.fr</a>&nbsp;et leur mise à jour assurée en autonomie&nbsp;<a rel='noreferrer' href='https://docs.pcrs.beta.gouv.fr/suivi-des-projets/edition-des-donnees' target='_blank' title='ouvre un onglet vers docs.pcrs.beta.gouv.fr'>par les porteurs de projets</a>.
     </div>
 
     <style jsx>{`
